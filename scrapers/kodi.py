7b608de86191a33252e2bc4a3dd3bec89ed6f24b
Phasetime <87231024+Phasetime@users.noreply.github.com>
2021-08-15 17:00:34 +0200

    updated info about rewriteBasePath #2

--- scrapers/kodi.py
+++ scrapers/kodi.py
@@ -9,10 +9,10 @@ import xml.etree.ElementTree as ET
 This script parses kodi nfo files for metadata. The .nfo file must be in the same directory as the video file and must be named exactly alike.
 """
 
-# If you want to ingest image data from the .nfo the path to these files may need to be rewritten. Especially when using a docker container.
+# If you want to ingest image files from the .nfo the path to these files may need to be rewritten. Especially when using a docker container.
 rewriteBasePath = False
 # Example: Z:\Videos\Studio_XXX\example_cover.jpg -> /data/Studio_XXX/example_cover.jpg
-basePathBefore = 'Z:'
+basePathBefore = 'Z:\Videos'
 basePathAfter = "/data"
 
 def query_xml(path, title):
