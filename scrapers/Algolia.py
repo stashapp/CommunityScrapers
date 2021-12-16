4a5dd0ff92d2a916b25b87e7352f3050fe4d1b80
Belleyy <Belleyy@users.noreply.github.com>
2021-12-16 10:10:35 +0100

    Update Algolia.py
    bnkai fix and changed my formatting

--- scrapers/Algolia.py
+++ scrapers/Algolia.py
@@ -19,24 +19,25 @@ import py_common.log as log
 
 # Print debug message.
 PRINT_DEBUG = True
-# Print ratio message. (Show title find in search)
+# Print ratio message. (Show title in search results)
 PRINT_MATCH = True
-# File used to store key to connect the API.
+# File to store the Algolia API key.
 STOCKAGE_FILE_APIKEY = "Algolia.ini"
 # Tag you always want in Scraper window.
 FIXED_TAGS = ""
 
 # Setup
 
+
 def check_db(DB_PATH, SCENE_ID):
     try:
         sqliteConnection = sqlite3.connect("file:" + DB_PATH + "?mode=ro", uri=True)
-        log.debug("Database successfully connected to SQLite")
+        log.debug("Connected to SQLite database")
     except:
-        log.warning("[Error] Fail to connect to the database")
+        log.warning("Fail to connect to the database")
         return None, None, None
     cursor = sqliteConnection.cursor()
-    cursor.execute("SELECT size,duration,height from scenes WHERE id=?;",[SCENE_ID])
+    cursor.execute("SELECT size,duration,height from scenes WHERE id=?;", [SCENE_ID])
     record = cursor.fetchall()
     database = {}
     database["size"] = int(record[0][0])
@@ -46,21 +47,24 @@ def check_db(DB_PATH, SCENE_ID):
     sqliteConnection.close()
     return database
 
+
 # General
 
+
 def sendRequest(url, head, json=""):
-    #log.debug("Request URL: {}".format(url))
-    response = requests.post(url, headers=head,json=json, timeout=10)
+    log.debug("Request URL: {}".format(url))
+    response = requests.post(url, headers=head, json=json, timeout=10)
     #log.debug("Returned URL: {}".format(response.url))
     if response.content and response.status_code == 200:
         return response
     else:
         log.warning("[REQUEST] Error, Status Code: {}".format(response.status_code))
-        print(response.text, file=open("request.html", "w", encoding='utf-8'))
+        #print(response.text, file=open("algolia_request.html", "w", encoding='utf-8'))
     return None
 
+
 # API Authentification
-def apikey_get(site_url,time):
+def apikey_get(site_url, time):
     r = sendRequest(site_url, HEADERS)
     if r is None:
         return None, None
@@ -73,7 +77,7 @@ def apikey_get(site_url,time):
         log.info("New API keys: {}".format(api_key))
         return application_id, api_key
     else:
-        log.error("Can't retrieve API keys from the html ({})".format(site_url))
+        log.error("Can't retrieve API keys from page ({})".format(site_url))
         return None, None
 
 
@@ -87,21 +91,24 @@ def check_config(domain, time):
         config = ConfigParser()
         config.read(STOCKAGE_FILE_APIKEY)
         try:
-            time_past = datetime.datetime.strptime(config.get(domain, 'date'), '%Y-%m-%d %H:%M:%S.%f')
+            time_past = datetime.datetime.strptime(
+                config.get(domain, 'date'), '%Y-%m-%d %H:%M:%S.%f')
 
-            if time_past.hour-1 < time.hour < time_past.hour+1 and (time - time_past).days == 0:
+            if time_past.hour - 1 < time.hour < time_past.hour + 1 and (time - time_past).days == 0:
                 log.debug("Using old key")
                 application_id = config.get(domain, 'app_id')
                 api_key = config.get(domain, 'api_key')
                 return application_id, api_key
             else:
-                log.info("Need new api key: [{}|{}|{}]".format(time.hour,time_past.hour,(time - time_past).days))
+                log.info(
+                    "Need new api key: [{}|{}|{}]".format(
+                        time.hour, time_past.hour, (time - time_past).days))
         except NoSectionError:
             pass
     return None, None
 
 
-def write_config(date,app_id,api_key):
+def write_config(date, app_id, api_key):
     log.debug("Writing config!")
     config = ConfigParser()
     config.read(STOCKAGE_FILE_APIKEY)
@@ -117,15 +124,15 @@ def write_config(date,app_id,api_key):
     return
 
 
-
 # API Search Data
 
-def api_search_req(type_search,query,api_url):
+
+def api_search_req(type_search, query, api_url):
     api_request = None
     if type_search == "query":
-        api_request = api_search_query(query,api_url)
+        api_request = api_search_query(query, api_url)
     if type_search == "id":
-        api_request = api_search_id(query,api_url)
+        api_request = api_search_id(query, api_url)
     if api_request:
         api_search = api_request.json()["results"][0].get("hits")
         if api_search:
@@ -133,16 +140,17 @@ def api_search_req(type_search,query,api_url):
     return None
 
 
-def api_search_id(url_id,api_url):
+def api_search_id(url_id, api_url):
     clip_id = ["clip_id:{}".format(url_id)]
     request_api = {
-        "requests": [
-            {
-                "indexName": "all_scenes",
-                "params": "query=&hitsPerPage=20&page=0",
-                "facetFilters": clip_id
-            }
-        ]
+        "requests":
+            [
+                {
+                    "indexName": "all_scenes",
+                    "params": "query=&hitsPerPage=20&page=0",
+                    "facetFilters": clip_id
+                }
+            ]
     }
     r = sendRequest(api_url, HEADERS, request_api)
     return r
@@ -150,12 +158,13 @@ def api_search_id(url_id,api_url):
 
 def api_search_query(query, api_url):
     request_api = {
-        "requests": [
-            {
-                "indexName": "all_scenes",
-                "params": "query=" + query + "&hitsPerPage=40&page=0"
-            }
-        ]
+        "requests":
+            [
+                {
+                    "indexName": "all_scenes",
+                    "params": "query=" + query + "&hitsPerPage=40&page=0"
+                }
+            ]
     }
     r = sendRequest(api_url, HEADERS, request_api)
     return r
@@ -163,29 +172,59 @@ def api_search_query(query, api_url):
 
 # Searching Result
 
+
 def json_parser(search_json, range_duration=60, single=False):
     result_dict = {}
     # Just for not printing the full JSON in log...
     debug_dict = {}
+    with open("adultime_scene_search.json", 'w', encoding='utf-8') as f:
+        json.dump(search_json, f, ensure_ascii=False, indent=4)
     for scene in search_json:
         r_match = match_result(scene, range_duration, single)
         if r_match["info"]:
             if result_dict.get(r_match["info"]):
                 # Url should be more accurate than the title
                 if r_match["url"] > result_dict[r_match["info"]]["url"]:
-                    result_dict[r_match["info"]] = {"title": r_match["title"],"url": r_match["url"],"json": scene}
-                    debug_dict[r_match["info"]] = {"title": r_match["title"],"url": r_match["url"],"scene": scene["title"]}
-                elif r_match["title"] > result_dict[r_match["info"]]["title"] and r_match["title"] > result_dict[r_match["info"]]["url"]:
-                    result_dict[r_match["info"]] = {"title": r_match["title"],"url": r_match["url"],"json": scene}
-                    debug_dict[r_match["info"]] = {"title": r_match["title"],"url": r_match["url"],"scene": scene["title"]}
+                    result_dict[r_match["info"]] = {
+                        "title": r_match["title"],
+                        "url": r_match["url"],
+                        "json": scene
+                    }
+                    debug_dict[r_match["info"]] = {
+                        "title": r_match["title"],
+                        "url": r_match["url"],
+                        "scene": scene["title"]
+                    }
+                elif r_match["title"] > result_dict[r_match["info"]]["title"] and r_match[
+                        "title"] > result_dict[r_match["info"]]["url"]:
+                    result_dict[r_match["info"]] = {
+                        "title": r_match["title"],
+                        "url": r_match["url"],
+                        "json": scene
+                    }
+                    debug_dict[r_match["info"]] = {
+                        "title": r_match["title"],
+                        "url": r_match["url"],
+                        "scene": scene["title"]
+                    }
             else:
-                result_dict[r_match["info"]] = {"title": r_match["title"],"url": r_match["url"],"json": scene}
-                debug_dict[r_match["info"]] = {"title": r_match["title"],"url": r_match["url"],"scene": scene["title"]}
+                result_dict[r_match["info"]] = {
+                    "title": r_match["title"],
+                    "url": r_match["url"],
+                    "json": scene
+                }
+                debug_dict[r_match["info"]] = {
+                    "title": r_match["title"],
+                    "url": r_match["url"],
+                    "scene": scene["title"]
+                }
     # Engine whoaaaaa
     # A = ByID/Most likely | S = Size | D = Duration | N = Network | R = Only Ratio
     log.info("--- BEST RESULT ---")
-    for key,item in debug_dict.items():
-        log.info("[{}] Title: {}; Ratio Title: {} - URL: {}".format(key,item["scene"],round(item["title"],3),round(item["url"],3)))
+    for key, item in debug_dict.items():
+        log.info(
+            "[{}] Title: {}; Ratio Title: {} - URL: {}".format(
+                key, item["scene"], round(item["title"], 3), round(item["url"], 3)))
     log.info("--------------")
     #
     if result_dict.get("ASDN"):
@@ -206,7 +245,7 @@ def json_parser(search_json, range_duration=60, single=False):
     elif result_dict.get("A"):
         if result_dict["A"]["title"] > 0.7 or result_dict["A"]["url"] > 0.7:
             return result_dict["A"]["json"]
-    # 
+    #
     elif result_dict.get("SDN"):
         return result_dict["SDN"]["json"]
     elif result_dict.get("SD"):
@@ -231,7 +270,7 @@ def json_parser(search_json, range_duration=60, single=False):
         if result_dict["R"]["title"] > 0.8 or result_dict["R"]["url"] > 0.8:
             return result_dict["R"]["json"]
     return None
-    
+
 
 def match_result(api_scene, range_duration=60, single=False):
     api_title = api_scene.get("title")
@@ -248,7 +287,7 @@ def match_result(api_scene, range_duration=60, single=False):
             if db_height == "2160":
                 api_filesize = api_scene["download_file_sizes"].get("4k")
             else:
-                api_filesize = api_scene["download_file_sizes"].get(db_height+"p")
+                api_filesize = api_scene["download_file_sizes"].get(db_height + "p")
             if api_filesize:
                 api_filesize = int(api_filesize)
         if api_filesize is None:
@@ -257,8 +296,8 @@ def match_result(api_scene, range_duration=60, single=False):
                 api_filesize = int(api_filesize)
         if db_duration - range_duration <= api_duration <= db_duration + range_duration:
             match_duration = True
-        db_size_max = db_size + (db_size/100)
-        db_size_min = db_size - (db_size/100)
+        db_size_max = db_size + (db_size / 100)
+        db_size_min = db_size - (db_size / 100)
         if api_filesize:
             if db_size_min <= api_filesize <= db_size_max:
                 match_size = True
@@ -276,28 +315,34 @@ def match_result(api_scene, range_duration=60, single=False):
 
     # Matching ratio
     if SCENE_TITLE:
-        match_ratio_title = difflib.SequenceMatcher(None, SCENE_TITLE.lower(), api_title.lower()).ratio()
+        match_ratio_title = difflib.SequenceMatcher(
+            None, SCENE_TITLE.lower(), api_title.lower()).ratio()
     else:
         match_ratio_title = 0
     if url_title and api_scene.get("url_title"):
-        match_ratio_title_url = difflib.SequenceMatcher(None, url_title.lower(), api_scene["url_title"].lower()).ratio()
+        match_ratio_title_url = difflib.SequenceMatcher(
+            None, url_title.lower(), api_scene["url_title"].lower()).ratio()
     else:
         match_ratio_title_url = 0
 
     # Rank search result
 
-    log.debug("[MATCH] Title: {} |-RATIO-| Title: {} / URL: {} |-MATCH-| Duration: {}, Size: {}, Domain: {}".format(api_title, round(match_ratio_title, 5), round(match_ratio_title_url, 5),match_duration, match_size, match_domain))
+    log.debug(
+        "[MATCH] Title: {} |-RATIO-| Title: {} / URL: {} |-MATCH-| Duration: {}, Size: {}, Domain: {}"
+        .format(
+            api_title, round(match_ratio_title, 5), round(match_ratio_title_url, 5),
+            match_duration, match_size, match_domain))
     match_dict = {}
     match_dict["title"] = match_ratio_title
     match_dict["url"] = match_ratio_title_url
     information_used = ""
-    if (single == True and (match_duration == True or (database_dict is None and match_ratio_title_url > 0.5))) or match_ratio_title_url == 1:
+    if (single and (match_duration or (database_dict is None and match_ratio_title_url > 0.5))) or match_ratio_title_url == 1:
         information_used += "A"
-    if match_size == True:
+    if match_size:
         information_used += "S"
-    if match_duration == True:
+    if match_duration:
         information_used += "D"
-    if match_domain == True:
+    if match_domain:
         information_used += "N"
     if information_used == "":
         information_used = "R"
@@ -307,6 +352,7 @@ def match_result(api_scene, range_duration=60, single=False):
 
 
 # Final
+
 
 def scraping_json(api_json, url=None):
     scrape = {}
@@ -323,11 +369,18 @@ def scraping_json(api_json, url=None):
     if api_json.get('serie_name'):
         scrape['studio']['name'] = api_json.get('serie_name')
 
-    log.debug("[STUDIO] {} - {} - {} - {}".format(api_json.get('serie_name'),api_json.get('network_name'),api_json.get('mainChannelName'),api_json.get('sitename_pretty')))
+    log.debug(
+        "[STUDIO] {} - {} - {} - {}".format(
+            api_json.get('serie_name'), api_json.get('network_name'),
+            api_json.get('mainChannelName'), api_json.get('sitename_pretty')))
     # Performer
     perf = []
     for x in api_json.get('actors'):
-        perf.append({"name": x.get('name').strip(), "gender":x.get('gender')})
+        if x.get('gender') == "female":
+            perf.append({
+                "name": x.get('name').strip(),
+                "gender": x.get('gender')
+            })
     scrape['performers'] = perf
 
     # Tags
@@ -350,20 +403,23 @@ def scraping_json(api_json, url=None):
         try:
             scrape['image'] = 'https://images03-fame.gammacdn.com/movies' + next(iter(api_json['pictures']['sfw']['top'].values()))
         except:
-            log.warning("Can't manage to get the image for some reason.")
+            log.warning("Can't locate image.")
     # URL
     try:
-        scrape['url'] = 'https://{}.com/en/video/{}/{}/{}'.format(api_json["sitename"], api_json["sitename"], api_json["url_title"], api_json["clip_id"])
+        scrape['url'] = 'https://{}.com/en/video/{}/{}/{}'.format(
+            api_json["sitename"], api_json["sitename"], api_json["url_title"],
+            api_json["clip_id"])
     except:
         if url:
             scrape['url'] = url
     #debug("{}".format(scrape))
     return scrape
 
+
 SITE = sys.argv[1]
 
 try:
-    USERFOLDER_PATH = re.match(r".+\.stash.",__file__).group(0)
+    USERFOLDER_PATH = re.match(r".+\.stash.", __file__).group(0)
     CONFIG_PATH = USERFOLDER_PATH + "config.yml"
     log.debug("Config Path: {}".format(CONFIG_PATH))
 except:
@@ -371,9 +427,12 @@ except:
     CONFIG_PATH = None
 
 HEADERS = {
-    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0',
-    "Origin": "https://www.{}.com".format(SITE),
-    "Referer": "https://www.{}.com".format(SITE)
+    "User-Agent":
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0',
+    "Origin":
+        "https://www.{}.com".format(SITE),
+    "Referer":
+        "https://www.{}.com".format(SITE)
 }
 
 FRAGMENT = json.loads(sys.stdin.read())
@@ -384,7 +443,6 @@ SCENE_URL = FRAGMENT.get("url")
 
 #log.debug(HEADERS)
 #log.debug('{}'.format(FRAGMENT))
-
 
 if "validName" in sys.argv and SCENE_URL is None:
     sys.exit(1)
@@ -415,24 +473,24 @@ if DB_PATH:
         database_dict = graphql.getScene(SCENE_ID)
         if database_dict is None:
             # Get data by SQlite
-            log.warning("Fail to use GraphQL, trying to read database directly.")
+            log.warning("GraphQL request failed, accessing database directly...")
             database_dict = check_db(DB_PATH, SCENE_ID)
         else:
             database_dict = database_dict["file"]
         log.debug("[DATABASE] Info: {}".format(database_dict))
     else:
         database_dict = None
-        log.warning("Can't use the database because URL Scraping.")
+        log.debug("URL scraping... Ignoring database...")
 else:
     database_dict = None
-    log.warning("Can't find the database.")
+    log.warning("Database path missing.")
 
 # Extract things
 url_title = None
 url_id = None
 url_domain = None
 if SCENE_URL:
-    url_domain = re.sub(r"www\.|\.com","",urlparse(SCENE_URL).netloc)
+    url_domain = re.sub(r"www\.|\.com", "", urlparse(SCENE_URL).netloc)
     log.info("URL Domain: {}".format(url_domain))
     url_id_check = re.sub('.+/', '', SCENE_URL)
     # Gettings ID
@@ -450,7 +508,7 @@ if SCENE_URL:
         log.info("URL_TITLE: {}".format(url_title))
     except:
         log.warning("Can't get url_title from URL")
-        
+
 # Filter title
 if SCENE_TITLE:
     SCENE_TITLE = re.sub(r'[-._\']', ' ', os.path.splitext(SCENE_TITLE)[0])
@@ -460,11 +518,10 @@ if SCENE_TITLE:
     SCENE_TITLE = re.sub(r'\s\d{2}\s\d{2}\s\d{2}|\s\d{4}\s\d{2}\s\d{2}', '', SCENE_TITLE)
     log.debug("Title: {}".format(SCENE_TITLE))
 
-
 # ACCESS API
 # Check existing API keys
 CURRENT_TIME = datetime.datetime.now()
-application_id, api_key = check_config(SITE,CURRENT_TIME)
+application_id, api_key = check_config(SITE, CURRENT_TIME)
 
 # Getting new key
 if application_id is None:
@@ -474,13 +531,14 @@ if application_id is None:
     sys.exit(1)
 
 # Time to search the API
-api_url = "https://tsmkfa364q-dsn.algolia.net/1/indexes/*/queries?x-algolia-application-id={}&x-algolia-api-key={}".format(application_id, api_key)
+api_url = "https://tsmkfa364q-dsn.algolia.net/1/indexes/*/queries?x-algolia-application-id={}&x-algolia-api-key={}".format(
+    application_id, api_key)
 api_search = None
 api_json = None
 
-
+# sceneByName
 if SEARCH_TITLE:
-    SEARCH_TITLE = SEARCH_TITLE.replace("."," ")
+    SEARCH_TITLE = SEARCH_TITLE.replace(".", " ")
     log.debug("[API] Searching for: {}".format(SEARCH_TITLE))
     api_search = api_search_req("query", SEARCH_TITLE, api_url)
     final_json = None
@@ -494,7 +552,7 @@ if SEARCH_TITLE:
         if result_search:
             final_json = result_search
     if final_json is None:
-        log.error("API Search don't give any result")
+        log.error("API Search finished. No results!")
     print(json.dumps(final_json))
     sys.exit()
 
@@ -527,3 +585,4 @@ if api_json:
 else:
     log.error("Can't find the scene")
     print(json.dumps(None))
+    sys.exit()
