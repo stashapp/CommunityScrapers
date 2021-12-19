2f1b976087858733c8653a53cce0ecd63132eaf4
Belleyy <Belleyy@users.noreply.github.com>
2021-12-19 11:18:52 +0100

    Algolia Python Scraper (Adultime, filthykings, zerotolerancefilms, ...) (#793)

--- scrapers/py_common/log.py
+++ scrapers/py_common/log.py
@@ -1,48 +1,39 @@
-# Log messages are transmitted via stderr and are
+import sys
+import re
+# Log messages sent from a script scraper instance are transmitted via stderr and are
 # encoded with a prefix consisting of special character SOH, then the log
-# level (one of t, d, i, w, e - corresponding to trace, debug, info,
+# level (one of t, d, i, w or e - corresponding to trace, debug, info,
 # warning and error levels respectively), then special character
 # STX.
 #
 # The log.trace, log.debug, log.info, log.warning, and log.error methods, and their equivalent
 # formatted methods are intended for use by script scraper instances to transmit log
 # messages.
+#
 
-import re, sys, copy, json
+def __log(level_char: bytes, s):
+    if level_char:
+        lvl_char = "\x01{}\x02".format(level_char.decode())
+        s = re.sub(r"data:image.+?;base64(.+?')","[...]",str(s))
+        for x in s.split("\n"):
+            print(lvl_char, x, file=sys.stderr, flush=True)
 
-def __prefix(levelChar):
-    startLevelChar = b'\x01'
-    endLevelChar = b'\x02'
-
-    ret = startLevelChar + levelChar + endLevelChar
-    return ret.decode()
-
-def __log(levelChar, s):
-    s_out = copy.deepcopy(s)
-    
-    if isinstance(s_out, dict):
-        s_out = json.dumps(s_out)
-
-    if not isinstance(s_out, str):
-        s_out = str(s_out)
-    s_out = re.sub(r'(?<=")(data:image.+?;base64).+?(?=")', r'\1;truncated', s_out)
-
-    if levelChar == "":
-        return
-
-    print(__prefix(levelChar) + s_out + "\n", file=sys.stderr, flush=True)
 
 def trace(s):
     __log(b't', s)
 
+
 def debug(s):
     __log(b'd', s)
+
 
 def info(s):
     __log(b'i', s)
 
+
 def warning(s):
     __log(b'w', s)
+
 
 def error(s):
     __log(b'e', s)
