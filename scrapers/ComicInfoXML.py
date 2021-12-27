1e30bdac47584025919289e2159fd2e0c17e0960
NozomiClambie <94254167+NozomiClambie@users.noreply.github.com>
2021-12-27 16:53:54 +1000

    Update ComicInfoXML.py

--- scrapers/ComicInfoXML.py
+++ scrapers/ComicInfoXML.py
@@ -1,18 +1,26 @@
-import os
 import sys
 import json
-import sqlite3
-import mimetypes
-import base64
 import xml.etree.ElementTree as ET
-"""
-This script parses xml files for metadata. The .xml file must be in the same directory as the gallery files and named either ComicInfo.xml for loose files (folder full oj jpg/png's) or the same name as the .cbz/.zip file
-"""
-debug = False
+import pathlib
 
-def query_xml(path, title):
-    tree=ET.parse(path)
+import py_common.graphql as graphql
+import py_common.log as log
+
+"""
+This script parses xml files for metadata. 
+The .xml file must be in the same directory as the gallery files and named either ComicInfo.xml for loose files (folder full oj jpg/png's) 
+or the same name as the .cbz/.zip file
+"""
+
+def query_xml(gallery_path, title):
     res={"title":title}
+    try:        
+        tree=ET.parse(gallery_path)
+    except Exception as e:
+        log.error(f'xml parsing failed:{e}')
+        print(json.dumps(res))
+        exit(1)
+    
     if tree.find("Title") != None:
         res["title"] = tree.find("Title").text
 
@@ -39,44 +47,35 @@ def query_xml(path, title):
 
     return res
 
-def debug(s):
-    if debug: print(s, file=sys.stderr)
-
-def get_file_path(gallery_id):
-    db_file = "../stash-go.sqlite"
-
-    con = sqlite3.connect(db_file)
-    cur = con.cursor()
-    for row in cur.execute("SELECT * FROM galleries where id = " + str(gallery_id) + ";"):
-        filepath = row[1]
-    con.close()
-    return filepath
-
 if sys.argv[1] == "query":
     fragment = json.loads(sys.stdin.read())
-    res = {"title": fragment["title"]}
-    videoFilePath = get_file_path(fragment["id"])
+    g_id = fragment.get("id")
+    if not g_id:
+        log.error(f"No ID found")
+        sys.exit(1)
 
-    # Check for an xml file that exists in the same folder with the same name as the .cbz or .zip
-    if "cbz" in videoFilePath:
-        temp = videoFilePath.rsplit('/', 1)
-        temp[-1] = "ComicInfo.xml"
-        xmlFilePath = "/".join(temp)
-        debug("CBZ Format, using: " + xmlFilePath)
-    elif "zip" in videoFilePath:
-        temp = videoFilePath.rsplit('/', 1)
-        temp[-1] = "ComicInfo.xml"
-        xmlFilePath = "/".join(temp)
-        debug("ZIP Format, using: " + xmlFilePath)
-    else:
-        # Use loose files format
-        xmlFilePath = videoFilePath+"/ComicInfo.xml"
-        debug("Folder format, using: " + xmlFilePath)
+    gallery = graphql.getGallery(g_id)
+    if gallery:
+        gallery_path = gallery.get("path")
+        if gallery_path:
+            p = pathlib.Path(gallery_path)
+            
+            res = {"title": fragment["title"]}
+            # Determine if loose file format or archive such as .cbz or .zip
+            if "cbz" in gallery_path or "zip" in gallery_path:
+                 # Look for filename.xml where filename.(cbz|zip) is the gallery
+                 f = p.with_suffix('.xml')
+                 log.debug(f"Single File Format, using: {f}")
+            else:
+                # Use loose files format
+                # Look for ComicInfo.xml in the gallery's folder
+                f = pathlib.Path(p.resolve(),"ComicInfo.xml")
+                log.debug(f"Folder format, using:{f}")
 
-    if os.path.isfile(xmlFilePath):
-        res = query_xml(xmlFilePath, fragment["title"])
-    else:
-        debug("No file found at: " + xmlFilePath)
+            if f.is_file():
+                res = query_xml(f, fragment["title"])
+            else:
+                log.warning(f'No xml files found for the gallery: {p}')
 
-    print(json.dumps(res))
-    exit(0)
+            print(json.dumps(res))
+            exit(0)
