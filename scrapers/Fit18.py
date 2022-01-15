523f34357cc1eb339e2425753fae76b13974e2a6
bnkai <48220860+bnkai@users.noreply.github.com>
2022-01-15 15:03:06 +0200

    Add Thicc18 support to Fit18 scraper (#851)

--- scrapers/Fit18.py
+++ scrapers/Fit18.py
@@ -1,162 +1,197 @@
-import base64
-import datetime
 import json
-import string
-import sys
 import re
+import sys
+import urllib.parse
 from urllib.parse import urlparse
-# extra modules below need to be installed
-import cloudscraper
-from lxml import html
 
-class Scraper:
-    def set_value(self,value):
-        if value:
-            if not re.match(r'(?i)no data', value[0]):
-                    return value[0]
+import requests
+
+try:
+    import py_common.log as log
+except ModuleNotFoundError:
+    print(
+        "You need to download the folder 'py_common' from the community repo (CommunityScrapers/tree/master/scrapers/py_common)",
+        file=sys.stderr)
+    sys.exit(1)
+
+
+class Site:
+
+    def __init__(self, name: str):
+        self.name = name
+        self.homepage = "https://" + self.name.lower() + ".com"
+        self.api = "https://" + self.name.lower() + ".team18.app/graphql"
+        if name == "Fit18":
+            self.api_key = "77cd9282-9d81-4ba8-8868-ca9125c76991"
+        elif name == "Thicc18":
+            self.api_key = "0e36c7e9-8cb7-4fa1-9454-adbc2bad15f0"
+
+    def isValidURL(self, url: str):
+        u = url.lower().rstrip("/")
+        up = urlparse(u)
+        if up.hostname is None:
+            return False
+        if up.hostname.lstrip("www.").rstrip(".com") == self.name.lower():
+            splits = u.split("/")
+            if len(splits) < 4:
+                return False
+            if splits[-2] == "videos":
+                self.id = urllib.parse.unquote(url).split("/")[-1]
+                ns = self.id.split(":")
+                if len(ns) < 2:
+                    return False
+                self.number = ns[-1]
+                return True
+        return False
+
+    def getScene(self, url: str):
+        log.debug(f"Scraping using {self.name} graphql API")
+        q = {"variables": {"videoId": self.id}, "query": self.getVideoQuery}
+        r = self.callGraphQL(q)
+        return self.parse_scene(r)
+
+    def getImage(self, talentId: str):
+        q = {
+            "variables": {
+                "paths": [
+                    f"/members/models/{talentId}/scenes/{self.number}/videothumb.jpg"
+                ]
+            },
+            "query": self.getAssetQuery
+        }
+        r = self.callGraphQL(q)
+        if r:
+            if r["asset"]["batch"].get("result"):
+                if r["asset"]["batch"]["result"][0].get("serve"):
+                    return r["asset"]["batch"]["result"][0]["serve"].get("uri")
         return None
 
-    def set_stripped_value(self,value):
-        if value:
-            return value[0].strip("\n ")
-        return None
+    def callGraphQL(self, query: dict):
+        headers = {
+            "Content-type": "application/json",
+            "argonath-api-key": self.api_key,
+            "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0",
+            "Origin": self.homepage,
+            "Referer": self.homepage
+        }
 
-    def set_concat_value(self,sep,values):
-        if values:
-            return sep.join(values)
-        return None
-
-    def set_named_value(self, name, value):
-        if value:
-            attr = { name: value[0] }
-            return attr
-        return None
-
-    def set_named_values(self, name, values):
-        res = []
-        for v in values:
-            r = { name: v }
-            res.append(r)
-        return res
-
-    def print(self):
-        for a in dir(self):
-            if not a.startswith('__') and not callable(getattr(self, a)) :
-                if vars(self)[a]:
-                    print("%s: %s" % (a , vars(self)[a] ) )
-
-    def to_json(self):
-        for a in dir(self):
-            if not a.startswith('__') and not callable(getattr(self, a)) :
-                if not vars(self)[a]:
-                    del vars(self)[a]
-        return json.dumps(self.__dict__)
-
-# Log messages are transmitted via stderr and are
-# encoded with a prefix consisting of special character SOH, then the log
-# level (one of t, d, i, w, e - corresponding to trace, debug, info,
-# warning and error levels respectively), then special character
-# STX.
-#
-# The LogTrace, LogDebug, LogInfo, LogWarning, and LogError methods, and their equivalent
-# formatted methods are intended for use by script scraper instances to transmit log
-# messages.
-#
-
-def __prefix(levelChar):
-    startLevelChar = b'\x01'
-    endLevelChar = b'\x02'
-
-    ret = startLevelChar + levelChar + endLevelChar
-    return ret.decode()
-
-def __log(levelChar, s):
-    if levelChar == "":
-        return
-
-    print(__prefix(levelChar) + s + "\n", file=sys.stderr, flush=True)
-
-def LogTrace(s):
-    __log(b't', s)
-
-def LogDebug(s):
-    __log(b'd', s)
-
-def LogInfo(s):
-    __log(b'i', s)
-
-def LogWarning(s):
-    __log(b'w', s)
-
-def LogError(s):
-    __log(b'e', s)
-
-def strip_end(text, suffix):
-    if suffix and text.endswith(suffix):
-        return text[:-len(suffix)]
-    return text
-
-def scrape(url):
-    scraper = cloudscraper.create_scraper()
-    try:
-        scraped = scraper.get(url, timeout=(3,7))
-    except Exception as e:
-        LogError("scrape error %s" % e)
-        sys.exit(1)
-    if scraped.status_code >= 400:
-        LogError('HTTP Error: %s' % scraped.status_code)
-        sys.exit(1)
-    return html.fromstring(scraped.content)
-
-def scrape_image(url):
-    scraper = cloudscraper.create_scraper()
-    try:
-        scraped = scraper.get(url, timeout=(3,7))
-    except Exception as e:
-        LogWarning("scrape error %s" %e )
-        return None
-    if scraped.status_code >= 400:
-        LogWarning('HTTP Error: %s' % scraped.status_code)
-        return None
-    b64img = base64.b64encode(scraped.content)
-    return "data:image/jpeg;base64," + b64img.decode('utf-8')
-
-def scene_from_tree(tree):
-    s = Scraper()
-
-    scene_title = tree.xpath('//div[contains(@class,"info")]/h1/text()')
-    s.title = s.set_stripped_value(scene_title)
-
-    scene_details = tree.xpath('//div[contains(@class,"info")]/p/text()')
-    s.details = s.set_value(scene_details)
-
-    scene_studio = ["Fit18"]
-    s.studio = s.set_named_value("name",scene_studio)
-
-    scene_performers = tree.xpath('//div[contains(@class,"info")]//span[contains(span,"model")]/a/text()')
-    s.performers = s.set_named_values("name", scene_performers)
-
-    scene_image_url = tree.xpath('//main/div/a/img/@src')
-    if scene_image_url:
         try:
-           LogDebug("downloading image from %s" % scene_image_url[0] )
-           s.image = scrape_image(scene_image_url[0])
-        except Exception as e:
-           LogDebug("error downloading image %s" %e)
+            response = requests.post(self.api,
+                                     json=query,
+                                     headers=headers,
+                                     timeout=10)
+            if response.status_code == 200:
+                result = response.json()
+                if result.get("error"):
+                    for error in result["error"]["errors"]:
+                        raise Exception(f"GraphQL error: {error}")
+                if result.get("data"):
+                    return result.get("data")
+            elif response.status_code >= 400:
+                sys.exit(f"HTTP Error {response.status_code}, {response.text}")
+            else:
+                raise ConnectionError(
+                    f"GraphQL query failed:{response.status_code} - {response.text}"
+                )
+        except Exception as err:
+            log.error(f"GraphqQL query failed {err}")
+            return None
 
-    res = s.to_json()
-    print(res)
-    sys.exit(0)
+    def parse_scene(self, response):
+        scene = {}
+        if response is None or response.get('video') is None:
+            return scene
 
-frag = json.loads(sys.stdin.read())
+        data = response["video"]["find"]["result"]
+        if data:
+            scene['title'] = data.get("title")
+            if data.get("description"):
+                d = None
+                if data["description"].get("short"):
+                    d = data["description"]["short"]
+                if data["description"].get("long"):
+                    d = data["description"]["long"]
+                # Remove double space
+                if d:
+                    d = re.sub(r" +", " ", d).strip()
+                    d = re.sub(r"^In 60FPS.\s*", "", d)
+                scene['details'] = d
 
-if not frag['url']:
-    LogWarning('No URL entered.')
+            perf_id = None
+            if data.get("talent"):
+                perf = None
 
-url = frag["url"]
-LogInfo("scraping %s" % url)
-tree = scrape(url)
+                # There are no 2 performers in a scene so useless to deal with lists
+                # performers = [x["talent"].get("name") for x in scene_info["talent"]]
+                perf = data["talent"][0]["talent"].get("name")
+                # Performer ID for getting the image
+                perf_id = data["talent"][0]["talent"].get("talentId")
+                if perf:
+                    scene['performers'] = [{"name": perf}]
 
-scene_from_tree(tree)
+            scene['studio'] = {'name': self.name}
+            if perf_id:
+                scene['image'] = self.getImage(perf_id)
 
-#Last Updated August 22, 2021
+            return scene
+        return None
+
+    getVideoQuery = """
+    query FindVideo($videoId: ID!) {
+        video {
+            find(input: { videoId: $videoId }) {
+                result {
+                    videoId
+                    title
+                    duration
+                    description {
+                        short
+                        long
+                    }
+                    talent {
+                        type
+                        talent {
+                            talentId
+                            name
+                        }
+                    }
+                }
+            }
+        }
+    }
+    """
+    getAssetQuery = """
+    query BatchFindAssetQuery($paths: [String!]!) {
+        asset {
+            batch(input: { paths: $paths }) {
+                result {
+                    path
+                    mime
+                    size
+                    serve {
+                        type
+                        uri
+                    }
+                }
+            }
+        }
+    }
+    """
+
+
+studios = {Site('Fit18'), Site('Thicc18')}
+fragment = json.loads(sys.stdin.read())
+url = fragment.get("url")
+
+if url:
+    for x in studios:
+        if x.isValidURL(url):
+            s = x.getScene(url)
+            #log.debug(f"{json.dumps(s)}")
+            print(json.dumps(s))
+            sys.exit(0)
+
+log.error(f"URL: {url} is not supported")
+print("{}")
+sys.exit(1)
