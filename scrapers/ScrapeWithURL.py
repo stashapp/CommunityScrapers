29db54e6dc1c964ab388bc35342ee24db1eecfa8
cj <72030708+Teda1@users.noreply.github.com>
2022-02-11 11:24:33 -0600

    Added ScrapeWithURL scrapper

---
+++ scrapers/ScrapeWithURL.py
@@ -0,0 +1,51 @@
+import json
+import sys
+
+import py_common.graphql as graphql
+import py_common.log as log
+
+SERVER_IP = "http://localhost:9999"
+# API key (Settings > Configuration > Authentication)
+STASH_API = ""
+SERVER_URL = SERVER_IP + "/graphql"
+
+def call_graphql(query, variables=None):
+    return graphql.callGraphQL(query, variables)
+
+def scrape_scene(url):
+    query = """query scrapeSceneURL($url: String!) {
+                  scrapeSceneURL(url: $url) {
+                        title
+                        details
+                        date
+                        image
+                        studio {
+                            name
+                        }
+                        tags {
+                            name
+                        }
+                        performers {
+                            name
+                        }
+                    }
+                }"""
+
+    variables = {'url': url}
+    result = call_graphql(query, variables)
+    log.info("result %s" % str(result))
+    return result["scrapeSceneURL"]
+
+
+FRAGMENT = json.loads(sys.stdin.read())
+SCENE_ID = FRAGMENT.get("id")
+
+scene = graphql.getScene(SCENE_ID)
+if scene:
+    scene_url = scene['url']
+
+    if scene_url:
+        result = scrape_scene(scene_url)
+        print(json.dumps(result))
+    else:
+        print(json.dumps({}))
