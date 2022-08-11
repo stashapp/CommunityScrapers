d97859d2c4b330055a4157cfd49f817341a4b899
selfhost-alt <91645868+selfhost-alt@users.noreply.github.com>
2022-08-11 09:23:03 -0700

    Remove unnecessary "pass"
    Co-authored-by: Belleyy <Belleyy@users.noreply.github.com>

--- scrapers/IAFD.py
+++ scrapers/IAFD.py
@@ -572,7 +572,6 @@ def scene_from_tree(tree):
             s.date = datetime.datetime.strptime(s.date, iafd_date_scene).strftime(stash_date)
         except:
             s.date = None
-            pass
 
     scene_details = tree.xpath('//div[@id="synopsis"]/div[@class="padded-panel"]//text()')
     s.details = s.set_value(scene_details)
