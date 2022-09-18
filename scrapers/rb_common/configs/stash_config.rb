49cd37680cb2f008829fe233c3537cc0c9aae8bd
bnkai <48220860+bnkai@users.noreply.github.com>
2022-09-18 13:08:47 +0300

    Update stash_config.rb

--- scrapers/rb_common/configs/stash_config.rb
+++ scrapers/rb_common/configs/stash_config.rb
@@ -5,7 +5,7 @@ require_relative "config_base"
 module Config
   class Stash < ConfigBase
     # Tweak user settings below. An API Key can be generated in Stash's setting page
-    # ( Settings > Configuration > Authentication )
+    # ( Settings > Security > Authentication )
     USER_CONFIG = {
       endpoint: "http://localhost:9999",
       api_key: ""
