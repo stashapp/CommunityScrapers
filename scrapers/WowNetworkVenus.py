f0e6a54ad14d5d46fdc8cad78353cda63260053e
estellaarrieta <111574945+estellaarrieta@users.noreply.github.com>
2022-09-23 10:09:22 +0300

    minor changes and cleanup
    for WOW_SUB_STUDIO_MAP.items() loop improvement
    typos and unnecessary leftover f-strings cleanup

--- scrapers/WowNetworkVenus.py
+++ scrapers/WowNetworkVenus.py
@@ -45,8 +45,7 @@ def search(title):
             scrape_all_results_pages(page_content, studio)
             log.debug(f'Searched {studio}, found {count_results_pages(studio)} pages')
         else: # filter results by wowgirls substudios
-            for studio_key in list(WOW_SUB_STUDIO_MAP.keys()):
-                studio = WOW_SUB_STUDIO_MAP[studio_key]
+            for studio_key, studio in WOW_SUB_STUDIO_MAP.items():
                 scraped = wow_sub_studio_filter_toggle(studio_key, query_studio)
                 page_content = html.fromstring(scraped.content)
                 scrape_all_results_pages(page_content, studio)
@@ -130,13 +129,13 @@ def output_json(title,tags,url,b64img,studio,performers):
 }
 
 def scene_card_parse(scene_card):
-    title = scene_card.xpath(f'./a[@class="title"]/text()')[0].strip()
-    imgurl = scene_card.xpath(f'.//img[@title]/@src')[0]
+    title = scene_card.xpath('./a[@class="title"]/text()')[0].strip()
+    imgurl = scene_card.xpath('.//img[@title]/@src')[0]
     imgurl= re.sub("_\w*", "_1280x720", imgurl)
     img = rGET(imgurl)
     b64img = base64.b64encode(img)
-    performers = scene_card.xpath(f'.//*[@class="models"]/a/text()')
-    tags = scene_card.xpath(f'.//span[@class="genres"]/a/text()')
+    performers = scene_card.xpath('.//*[@class="models"]/a/text()')
+    tags = scene_card.xpath('.//span[@class="genres"]/a/text()')
     return title, b64img, performers, tags
 
 def get_all_results(): #parse all scene elements, return all
@@ -144,9 +143,9 @@ def get_all_results(): #parse all scene elements, return all
     for studio, pages in search_results.items():
         query_studio = studio.replace(" ", "").lower()
         for page in pages:
-            scene_cards = page.xpath(f'//div[contains(@class, "ct_video")]//img[@title]/ancestor::div')
+            scene_cards = page.xpath('//div[contains(@class, "ct_video")]//img[@title]/ancestor::div')
             for scene_card in scene_cards:
-                url = f'https://venus.{query_studio}.com' + scene_card.xpath(f'./a/@href')[0]
+                url = f'https://venus.{query_studio}.com'+scene_card.xpath('./a/@href')[0]
                 title, b64img, performers, tags = scene_card_parse(scene_card)
                 parsed_scenes.append(output_json(title,tags,url,b64img,studio,performers))
     return parsed_scenes
@@ -158,10 +157,10 @@ def get_scene_with_id(sceneID): #parse all scene elements, return single with ma
             scene_cards_with_ID = page.xpath(f'//div[contains(@class, "ct_video")]//a[contains(@href,"{sceneID}")]/ancestor::div')
             if scene_cards_with_ID:
                 scene_card=scene_cards_with_ID[0]
-                url = f'https://venus.{query_studio}.com' + scene_card.xpath(f'./a/@href')[0]
+                url = f'https://venus.{query_studio}.com' + scene_card.xpath('./a/@href')[0]
                 title, b64img, performers, tags = scene_card_parse(scene_card)
                 return output_json(title,tags,url,b64img,studio,performers)
-    log.error('Scene not found!\nSome scenes doest not appear is search results unless you are logged in!')
+    log.error('Scene not found!\nSome scenes does not appear is search results unless you are logged in!')
     sys.exit()
 
 def interleave_results(parsed_scenes): #interleave search results by studio
@@ -206,6 +205,7 @@ search_results = {}
 s = requests.Session()
 s.headers.update({'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'})
 s.proxies.update(PROXIES)
+
 if NAME:
     search(NAME)
     parsed_scenes = get_all_results()
