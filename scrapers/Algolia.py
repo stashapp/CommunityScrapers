f85226a034d6871de2cdf2da4099bbd32ef29e45
bnkai <48220860+bnkai@users.noreply.github.com>
2022-10-02 11:40:08 +0300

    Move Girlfriendsfilms to an Algolia scraper

--- scrapers/Algolia.py
+++ scrapers/Algolia.py
@@ -43,6 +43,7 @@ MOVIE_SITES = {
     "devilsfilm": "https://www.devilsfilm.com/en/dvd",
     "diabolic": "https://www.diabolic.com/en/movie",
     "evilangel": "https://www.evilangel.com/en/movie",
+    "girlfriendsfilms": "https://www.girlfriendsfilms.com/en/movie",
     "outofthefamily": "https://www.outofthefamily.com/en/dvd",
     "peternorth": "https://www.peternorth.com/en/dvd",
     "tsfactor": "https://www.tsfactor.com/en/movie/",
@@ -570,6 +571,7 @@ try:
 except:
     USERFOLDER_PATH = None
     CONFIG_PATH = None
+    log.debug("No config")
 
 SITE = sys.argv[1]
 HEADERS = {
