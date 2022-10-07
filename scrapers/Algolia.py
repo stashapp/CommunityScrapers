43b688c43ce1beb46d13dcfa88154f1b8da5ad14
aussiehuddo <andy_huddo@hotmail.com>
2022-10-08 00:46:50 +1100

    Add lewood.com to Algolia_EvilAngel scraper (#1153)

--- scrapers/Algolia.py
+++ scrapers/Algolia.py
@@ -43,6 +43,7 @@ MOVIE_SITES = {
     "devilsfilm": "https://www.devilsfilm.com/en/dvd",
     "diabolic": "https://www.diabolic.com/en/movie",
     "evilangel": "https://www.evilangel.com/en/movie",
+    "lewood": "https://www.lewood.com/en/movie",
     "outofthefamily": "https://www.outofthefamily.com/en/dvd",
     "peternorth": "https://www.peternorth.com/en/dvd",
     "tsfactor": "https://www.tsfactor.com/en/movie/",
