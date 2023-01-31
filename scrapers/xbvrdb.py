919aea9cb05776602cb01afca9dda7fa66dacadf
vt-idiot <81622808+vt-idiot@users.noreply.github.com>
2023-01-31 12:30:14 -0500

    Update xbvrdb.py
    Actually making it work properly without requiring any GraphQL calls at all. Note: I have no idea if the .zip/gallery part still works, I never used it, and this scraper never worked for me since Stash didn't actually pre-populate the title fields with the file extensions. This fixes that.

--- scrapers/xbvrdb.py
+++ scrapers/xbvrdb.py
@@ -10,7 +10,7 @@ from os import path
    '''
 def lookup_scene(id):
     c=conn.cursor()
-    c.execute('select title,synopsis,site,cover_url,scene_url,date(release_date, "localtime") from scenes where id=?',(id,))
+    c.execute('SELECT title,synopsis,site,cover_url,scene_url,date(release_date, "localtime") FROM scenes WHERE id=?',(id,))
     row=c.fetchone()
     res={}
     res['title']=row[0]
@@ -19,20 +19,20 @@ def lookup_scene(id):
     res['image']=row[3]
     res['url']=row[4]
     res['date']=row[5]
-    c.execute("select tags.name from scene_tags,tags where scene_tags.tag_id=tags.id and scene_tags.scene_id=? ;",(id,))
+    c.execute("SELECT tags.name FROM scene_tags,tags WHERE scene_tags.tag_id=tags.id AND scene_tags.scene_id=? ;",(id,))
     row = c.fetchall()
     res['tags']=[{"name":x[0]} for x in row]
-    c.execute("select actors.name from scene_cast,actors where actors.id=scene_cast.actor_id and scene_cast.scene_id=? ;",(id,))
+    c.execute("SELECT actors.name FROM scene_cast,actors WHERE actors.id=scene_cast.actor_id AND scene_cast.scene_id=? ;",(id,))
     row = c.fetchall()
     res['performers']=[{"name":x[0]} for x in row]
     return res
 
 def find_scene_id(title):
     c = conn.cursor()
-    c.execute('SELECT scene_id FROM files WHERE filename=?', (title,))
+    c.execute('SELECT scene_id FROM files WHERE filename LIKE ?', (title+'____',))
     id=c.fetchone()
     if id == None:
-        c.execute('SELECT id FROM scenes WHERE scene_id=?', (title,))
+        c.execute('SELECT id FROM scenes WHERE scene_id LIKE ?', (title,))
         id = c.fetchone()
         if id is not None:
             return id[0]
@@ -48,7 +48,7 @@ def find_scene_id(title):
             if 'originals' in t:
                 t.remove('originals')
             title='%'.join(t)+'%'
-        c.execute('select id from scenes where title like ?',(title+'%',))
+        c.execute('SELECT id FROM scenes WHERE title LIKE ?',(title+'%',))
         id=c.fetchone()
         if id is not None:
             return id[0]
@@ -61,7 +61,6 @@ if not path.exists("xbvr.db"):
     print("Copy this database from the docker container and give it the name xbvr.db",file=sys.stderr)
     print("docker cp xbvr:/root/.config/xbvr/main.db xbvr.db",file=sys.stderr)
     exit(1)
-
 
 conn = sqlite3.connect('xbvr.db',detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
 
@@ -90,3 +89,4 @@ elif sys.argv[1] == "gallery_query":
         result.pop("image",None)
         print(json.dumps(result))
     conn.close()
+    
