debc45bb1001920c9a8327914a988332e86f2cf9
nrg101 <nrg101@macbook>
2023-03-09 01:43:17 +0000

    add python scraper and unit test for args/fragment

---
+++ scrapers/py_tests/test_python_generic.py
@@ -0,0 +1,63 @@
+'''
+Unit test for python_generic.py
+
+You can run this test with the following command:
+
+python3 -m unittest -v -b scrapers/py_tests/test_python_generic.py
+'''
+import inspect
+import json
+import os
+import sys
+import unittest
+from unittest.mock import patch
+
+from . import base_test_case
+
+# add parent directory (i.e. the scrapers directory) as a Python modules path
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+# now we can import the scraper module from the parent directory
+import python_generic
+
+FRAGMENT_URL = {"url": "http://domain"}
+
+class TestPythonScraper(base_test_case.BaseTestCase):
+    
+    def test_class_init_with_no_args(self):
+        # given
+        # no script arguments, no fragment input
+        testargs = ["script_name"]
+        scraper = None
+        with patch.object(sys, 'argv', testargs):
+            # then
+            with self.assertRaises(SystemExit):
+                # when
+                scraper = python_generic.PythonScraper()
+
+        # then
+        self.assertIsNone(scraper)
+
+    @patch('builtins.input', side_effect=[json.dumps(FRAGMENT_URL)])
+    def test_class_init_with_valid_args_and_valid_stdin(self, raw_input):
+        # given
+        # one script argument, the positional first argument 'action'
+        testargs = ["script_name", "sceneByURL"]
+        with patch.object(sys, 'argv', testargs):
+            # when
+            scraper = python_generic.PythonScraper()
+
+        # then
+        self.assertIsInstance(scraper, python_generic.PythonScraper)
+        self.assertHasAttr(scraper, '__init__')
+        self.assertHasAttr(scraper, 'args')
+        self.assertHasAttr(scraper, 'fragment')
+        self.assertHasAttr(scraper, 'start_processing')
+        self.assertIsNotNone(scraper.args)
+        self.assertDictEqual(scraper.fragment, FRAGMENT_URL)
+        self.assertEqual(scraper.args.action, 'sceneByURL')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ scrapers/python_generic.py
@@ -0,0 +1,62 @@
+import argparse
+import json
+import select
+import sys
+
+try:
+    from py_common import log
+except ModuleNotFoundError:
+    print(
+        "You need to download the folder 'py_common' from the community repo! "
+        "(CommunityScrapers/tree/master/scrapers/py_common)",
+        file=sys.stderr)
+    sys.exit(1)
+
+class PythonScraper:
+    args: argparse.Namespace = None
+    fragment: dict = {}
+
+    '''
+    Generic scraper to investigate interaction with stashapp
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        try:
+            self.__load_arguments()
+            self.__load_fragment()
+        except Exception as ex:
+            log.debug(ex)
+            log.error('Scraper initialisation failed. Exiting.')
+            sys.exit(1)
+    
+    def __load_arguments(self):
+        '''
+        Get the script arguments (specified in the YAML)
+        '''
+        parser = argparse.ArgumentParser(description='Script argument parser')
+        parser.add_argument('action', help='Action for the script to perform')
+        self.args = parser.parse_args()
+    
+    def __load_fragment(self):
+        '''
+        Get the JSON scene fragment from stdin (this is how stashapp sends it)
+        '''
+        self.fragment = json.loads(input())
+        log.debug(f"fragment: {self.fragment}")
+    
+    def start_processing(self):
+        '''
+        Start processing the scraper with the supplied arguments and input
+        '''
+        pass
+
+
+if __name__ == '__main__':
+    # instantiate class
+    scraper = PythonScraper()
+
+    # start processing
+    scraper.start_processing()
