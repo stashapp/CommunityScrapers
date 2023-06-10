1ea3e18adf84927d0d3bac68b2ba8d28bb24f7c8
DreamySleepyNightySnoozySnooze <the.g.banger.is.coming@gmail.com>
2023-06-11 09:52:35 +1000

    Add whispar wdtv metadata scraper

---
+++ scrapers/WhisparrWDTV.py
@@ -0,0 +1,87 @@
+import sys
+import pathlib
+
+import mimetypes
+import base64
+
+import json
+import xml.etree.ElementTree as ET
+
+import py_common.graphql as graphql
+import py_common.log as log
+"""  
+This script parses kodi nfo files for metadata. The .nfo file must be in the same directory as the video file and must be named exactly alike.
+"""
+
+# If you want to ingest image files from the .nfo the path to these files may need to be rewritten. Especially when using a docker container.
+rewriteBasePath = False
+# Example: Z:\Videos\Studio_XXX\example_cover.jpg -> /data/Studio_XXX/example_cover.jpg
+basePathBefore = 'Z:\Videos'
+basePathAfter = "/data"
+
+def query_xml(path, title):
+    res = {"title": title}
+    try:        
+        tree = ET.parse(path)
+    except Exception as e:
+        log.error(f'xml parsing failed:{e}')
+        print(json.dumps(res))
+        exit(1)
+    
+    if title == tree.find("episode_name").text:
+        log.info("Exact match found for " + title)
+    else:
+        log.info("No exact match found for " + title + ". Matching with " + tree.find("title").text + "!")
+    
+    # Extract matadata from xml
+    if tree.find("episode_name") != None:
+        res["title"] = tree.find("episode_name").text
+    
+    if tree.find("overview") != None:
+        res["details"] = tree.find("overview").text
+    
+    if tree.find("firstaired") != None:
+        res["date"] = tree.find("firstaired").text
+    
+    if tree.find("actor") != None and tree.find("actor").text:
+        res["performers"] = []
+        for actor in tree.find("actor").text.split(" / "):
+            res["performers"].append({"name": actor.split(" - ")[0]})
+    
+    if tree.find("series_name") != None:
+        res["studio"] = {"name":tree.find("series_name").text}
+    
+    return res
+
+def make_image_data_url(image_path):
+    # type: (str,) -> str
+    mime, _ = mimetypes.guess_type(image_path)
+    with open(image_path, 'rb') as img:
+        encoded = base64.b64encode(img.read()).decode()
+    return 'data:{0};base64,{1}'.format(mime, encoded)
+
+if sys.argv[1] == "query":
+    fragment = json.loads(sys.stdin.read())
+    s_id = fragment.get("id")
+    if not s_id:
+        log.error(f"No ID found")
+        sys.exit(1)
+    
+    # Assume that .nfo/.xml is named exactly alike the video file and is at the same location
+    # Query graphQL for the file path
+    scene = graphql.getScene(s_id)
+    if scene:
+        scene_path = scene.get("path")
+        if scene_path:
+            p = pathlib.Path(scene_path)
+            
+            res = {"title": fragment["title"]}
+            
+            f = p.with_suffix(".xml")
+            if f.is_file():
+                res = query_xml(f, fragment["title"])
+            else:
+                log.info(f"No nfo/xml files found for the scene: {p}")
+            
+            print(json.dumps(res))
+            exit(0)
