7546ccda6d7a4a8b3f49c8583ec4e020377ec7eb
Maista6969 <glad.beach5807@fastmail.com>
2023-08-22 15:16:10 +0200

    Update ScrapeWithURL for new scene structure
    Also makes it only use the URL and not needlessly query the GQL API for a scene when it already has everything it needs from the fragment

--- scrapers/ScrapeWithURL.py
+++ scrapers/ScrapeWithURL.py
@@ -5,47 +5,80 @@ try:
     import py_common.graphql as graphql
     import py_common.log as log
 except ModuleNotFoundError:
-    print("You need to download the folder 'py_common' from the community repo! (CommunityScrapers/tree/master/scrapers/py_common)", file=sys.stderr)
+    print(
+        "You need to download the folder 'py_common' from the community repo! (CommunityScrapers/tree/master/scrapers/py_common)",
+        file=sys.stderr,
+    )
     sys.exit()
+
 
 def call_graphql(query, variables=None):
     return graphql.callGraphQL(query, variables)
 
-def scrape_scene(url):
-    query = """query scrapeSceneURL($url: String!) {
-                  scrapeSceneURL(url: $url) {
-                        title
-                        details
-                        date
-                        image
-                        studio {
-                            name
-                        }
-                        tags {
-                            name
-                        }
-                        performers {
-                            name
-                        }
-                        url
-                    }
-                }"""
 
-    variables = {'url': url}
+def scrape_scene(url):
+    query = """
+query scrapeSceneURL($url: String!) {
+    scrapeSceneURL(url: $url) {
+        title
+        details
+        code
+        date
+        image
+        urls
+        studio {
+            name
+            url
+            image
+            parent {
+                name
+                url
+                image
+            }
+        }
+        tags {
+            name
+        }
+        performers {
+            aliases
+            birthdate
+            career_length
+            country
+            death_date
+            details
+            ethnicity
+            eye_color
+            fake_tits
+            gender
+            hair_color
+            height
+            instagram
+            images
+            measurements
+            name
+            piercings
+            tags {
+                name
+            }
+            tattoos
+            twitter
+            url
+            weight
+        }
+    }
+}"""
+
+    variables = {"url": url}
     result = call_graphql(query, variables)
     log.debug(f"result {result}")
     return result["scrapeSceneURL"]
 
 
 FRAGMENT = json.loads(sys.stdin.read())
-SCENE_ID = FRAGMENT.get("id")
+url = FRAGMENT.get("url")
 
-scene = graphql.getScene(SCENE_ID)
-if scene:
-    scene_url = scene['url']
-
-    if scene_url:
-        result = scrape_scene(scene_url)
-        print(json.dumps(result))
-    else:
-        print(json.dumps({}))
+if url:
+    result = scrape_scene(url)
+    print(json.dumps(result))
+else:
+    print(json.dumps({}))
