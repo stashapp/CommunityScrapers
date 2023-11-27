413280c4b2467df3a3bbc7efb8f78afdd65b29b8
MrX292 <42774880+MrX292@users.noreply.github.com>
2023-11-27 05:49:56 +0100

    Add files via upload
    use py_common.graphql and move config options into a separate file

---
+++ scrapers/ShokoAPI/config.py
@@ -0,0 +1,9 @@
+#it gets your Shoko api key with your shoko server username and password
+SHOKO = {
+    "url":
+        "http://localhost:8111", #your shoko server url
+    "user":
+        "username",#your shoko server username
+    "pass":
+        "password" #your shoko server password
+}
