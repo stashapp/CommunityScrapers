82e246b08595e014fa49443690145988f7cbe7c4
Maista6969 <glad.beach5807@fastmail.com>
2023-12-19 22:49:33 +0100

    Fix relative import in MissaX

--- scrapers/MissaX/MissaX.py
+++ scrapers/MissaX/MissaX.py
@@ -2,9 +2,18 @@
 import base64
 import datetime
 import json
+import os
 import re
 import sys
 import urllib.parse
+
+# to import from a parent directory we need to add that directory to the system path
+csd = os.path.dirname(os.path.realpath(__file__))  # get current script directory
+parent = os.path.dirname(csd)  #  parent directory (should be the scrapers one)
+sys.path.append(
+    parent
+)  # add parent dir to sys path so that we can import py_common from ther
+
 # extra modules below need to be installed
 try:
     import py_common.log as log
