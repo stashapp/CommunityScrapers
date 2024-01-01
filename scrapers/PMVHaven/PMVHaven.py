5dcf48b6b88dd2c6480c3c0577f7af833ba526c8
dopepump <155258061+dopepump@users.noreply.github.com>
2024-01-01 17:00:40 +0000

    Update PMVHaven.py

--- scrapers/PMVHaven/PMVHaven.py
+++ scrapers/PMVHaven/PMVHaven.py
@@ -4,24 +4,20 @@ import sys
 import requests
 
 try:
-    from py_common import log
+    import py_common.log as log
 except ModuleNotFoundError:
-    print("You need to download the folder 'py_common' from the community repo! (CommunityScrapers/tree/master/scrapers/py_common)", file=sys.stderr)
-    sys.exit()
+    print(
+        "You need to download the folder 'py_common' from the community repo (CommunityScrapers/tree/master/scrapers/py_common)",
+        file=sys.stderr)
+    sys.exit(1)
 
-# to import from a parent directory we need to add that directory to the system path
-csd = os.path.dirname(os.path.realpath(__file__))  # get current script directory
-parent = os.path.dirname(csd)  # parent directory (should be the scrapers one)
-sys.path.append(
-    parent
-)  # add parent dir to sys path so that we can import py_common from there
-
-#bugfix for socks5 proxies, due to pySocks implementation incompatibility with Stash
-proxy = os.environ.get('HTTPS_PROXY', '')
-if proxy != "" and proxy.startswith("socks5://"):
-    proxy = proxy.replace("socks5://", "socks5h://")
-    os.environ['HTTPS_PROXY'] = proxy
-    os.environ['HTTP_PROXY'] = proxy
+try:
+    from lxml import html
+except ModuleNotFoundError:
+    print("You need to install the lxml module. (https://lxml.de/installation.html#installation)", file=sys.stderr)
+    print("If you have pip (normally installed with python), run this command in a terminal (cmd): pip install lxml",
+          file=sys.stderr)
+    sys.exit(1)
 
 def fail(message):
     log.error(message)
@@ -35,7 +31,7 @@ def getData(sceneId):
             "view": True
         })
     except Exception as e:
-        fail(f"Error fetching data from PMVHaven API: {e}")
+        sys.exit(f"Error fetching data from PMVHaven API: {e}")
     return req.json()
 
 def getIMG(video):
@@ -46,18 +42,18 @@ def getIMG(video):
 
 '''    
     This assumes a URL of https://pmvhaven.com/video/{title}_{alphanumericVideoId}
-    As of 2023-12-31, this is the only valid video URL format. If this changes in
+    As of 2024-01-01, this is the only valid video URL format. If this changes in
     the future (i.e. more than one valid URL type, or ID not present in URL) and
     requires falling back to the old cloudscraper method, an xpath of 
         //meta[@property="video-id"]/@content 
     can be used to pass into the PMVHaven API
 '''
 
-def main():
+def sceneByURL():
     params = json.loads(sys.stdin.read())
 
     if not params['url']:
-        fail('No URL entered.')
+        fail('No URL entered')
 
     sceneId = params['url'].split('_')[-1]
 
@@ -67,13 +63,13 @@ def main():
     data = getData(sceneId)
 
     if not 'video' in data or len(data['video']) < 1:
-        fail(f"Video data not found in data: {data}")
+        fail(f"Video data not found in API response: {data}")
 
     video = getData(sceneId)['video'][0]
 
     tags = video['tags'] + video['categories']
 
-    ret = {
+    return {
         'title': video['title'],
         'image': getIMG(video),
         'date': video['isoDate'].split('T')[0],
@@ -92,7 +88,6 @@ def main():
             } for x in video['stars']
         ]
     }
-    print(json.dumps(ret))
 
-if __name__ == "__main__":
-    main()
+if sys.argv[1] == 'sceneByURL':
+    print(json.dumps(sceneByURL()))
