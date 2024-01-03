e279baabf4b0d549159b05fbe09d3b59c87333fe
JackDawson94 <100418085+JackDawson94@users.noreply.github.com>
2024-01-03 17:09:39 +0100

    Bufix

--- scrapers/PMVHaven/PMVHaven.py
+++ scrapers/PMVHaven/PMVHaven.py
@@ -2,22 +2,12 @@ import os
 import json
 import sys
 import requests
-import random
-import time
 from urllib.parse import urlparse
-# extra modules below need to be installed
-try:
-    import cloudscraper
-except ModuleNotFoundError:
-    print("You need to install the cloudscraper module. (https://pypi.org/project/cloudscraper/)", file=sys.stderr)
-    print("If you have pip (normally installed with python), run this command in a terminal (cmd): pip install cloudscraper", file=sys.stderr)
-    sys.exit()
 
 try:
-    from lxml import html
+    from py_common import log
 except ModuleNotFoundError:
-    print("You need to install the lxml module. (https://lxml.de/installation.html#installation)", file=sys.stderr)
-    print("If you have pip (normally installed with python), run this command in a terminal (cmd): pip install lxml", file=sys.stderr)
+    print("You need to download the folder 'py_common' from the community repo! (CommunityScrapers/tree/master/scrapers/py_common)", file=sys.stderr)
     sys.exit()
 
 # to import from a parent directory we need to add that directory to the system path
@@ -27,12 +17,6 @@ sys.path.append(
     parent
 )  # add parent dir to sys path so that we can import py_common from there
 
-try:
-    from py_common import log
-except ModuleNotFoundError:
-    print("You need to download the folder 'py_common' from the community repo! (CommunityScrapers/tree/master/scrapers/py_common)", file=sys.stderr)
-    sys.exit()
-
 #bugfix for socks5 proxies, due to pySocks implementation incompatibility with Stash
 proxy = os.environ.get('HTTPS_PROXY', '')
 if proxy != "" and proxy.startswith("socks5://"):
@@ -40,55 +24,19 @@ if proxy != "" and proxy.startswith("socks5://"):
     os.environ['HTTPS_PROXY'] = proxy
     os.environ['HTTP_PROXY'] = proxy
 
-URL_XPATH = '//meta[@property="og:video:url"]/@content'
-URL_XPATH_2 = '//meta[@property="og:video:secure_url"]/@content'
-IMAGE_XPATH = '//meta[@property="og:image"]/@content'
-
-def getHTML(url, retries=0):
-    scraper = cloudscraper.create_scraper()
-    
-    try:
-        scraped = scraper.get(url)
-    except requests.exceptions.Timeout as exc_time:
-        log.debug(f"Timeout: {exc_time}")
-        return getHTML(url, retries + 1)
-    except Exception as e:
-        log.error(f"scrape error {e}")
-        sys.exit(1)
-    if scraped.status_code >= 400:
-        if retries < 10:
-            wait_time = random.randint(1, 4)
-            log.debug(f"HTTP Error: {scraped.status_code}, waiting {wait_time} seconds")
-            time.sleep(wait_time)
-            return getHTML(url, retries + 1)
-        log.error(f"HTTP Error: {scraped.status_code}, giving up")
-        sys.exit(1)
-
-    return html.fromstring(scraped.text)
-
-def getXPATH(pageTree, XPATH):
-    res = pageTree.xpath(XPATH)
-    if res:
-        return res[0]
-    return ""
-
 def getData(sceneId):
     try:
+        log.debug(sceneId)
         req = requests.post("https://pmvhaven.com/api/v2/videoInput", json={
             "video": sceneId,
             "mode": "InitVideo",
             "view": True
         })
+        log.debug(req.json())
     except Exception as e:
         log.error(f"scrape error {e}")
         sys.exit(1)
     return req.json()
-
-def getURL(pageTree):
-    url = getXPATH(pageTree, URL_XPATH)
-    if not url:
-        return getXPATH(pageTree, URL_XPATH_2)
-    return url
 
 def getIMG(data):
     for item in data['thumbnails']:
@@ -102,8 +50,7 @@ def main():
         log.error('No URL entered.')
         sys.exit(1)
     
-    tree = getHTML(params['url'])
-    data = getData(getURL(tree).split('_')[-1])['video'][0]
+    data = getData(params['url'].split('_')[-1])['video'][0]
 
     tags = data['tags'] + data['categories']
 
