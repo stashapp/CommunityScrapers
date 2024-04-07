25612f8f8865d29622cf93ebf86f0e62366510c1
Maista6969 <glad.beach5807@fastmail.com>
2024-04-07 18:03:04 +0200

    Add scraper for Windows file attributes

--- scrapers/FileMetadata/FileMetadata.py
+++ scrapers/FileMetadata/FileMetadata.py
@@ -1,82 +1,83 @@
 import json
-import os
 import sys
 import subprocess as sp
 from datetime import datetime
 from urllib.parse import urlparse
 
-# to import from a parent directory we need to add that directory to the system path
-csd = os.path.dirname(os.path.realpath(__file__))  # get current script directory
-parent = os.path.dirname(csd)  #  parent directory (should be the scrapers one)
-sys.path.append(
-    parent
-)  # add parent dir to sys path so that we can import py_common from ther
+from py_common import graphql
+from py_common import log
 
-try:
-    from py_common import graphql
-    from py_common import log
-except ModuleNotFoundError:
-    print(
-        "You need to download the folder 'py_common' from the community repo! (CommunityScrapers/tree/master/scrapers/py_common)",
-        file=sys.stderr,
-    )
-    sys.exit()
 
-def format_date(date):
-    return datetime.strptime(date, '%Y%m%d').strftime('%Y-%m-%d')
-
-def parse_url_from_comment(comment):
+def parse_url(comment):
     if urlparse(comment).scheme:
         return comment
-    
+
     return None
 
-def metadata_from_primary_path(js):
-    scene_id = js["id"]
-    scene = graphql.getScene(scene_id)
-    scraped_metadata = {}
 
-    if scene is None:
-        log.error(f"Could not scrape video: scene not found - {scene_id}")
+def get_path(scene_id):
+    scene = graphql.getScene(scene_id)
+    if not scene:
+        log.error(f"Scene with ID '{scene_id}' not found")
         return
-    
-    path = scene["files"][0]["path"]
-    if path is None:
-        log.error("Could not scrape video: no file path")
+
+    if not scene["files"]:
+        log.error(f"Scene with ID '{scene_id}' has no files")
         return
-    
-    video_data = sp.run(["ffprobe", "-loglevel", "error", "-show_entries", "format_tags", "-of", "json", f"{path}"], capture_output=True).stdout
-    if video_data is None:
+
+    return scene["files"][0]["path"]
+
+
+def scrape_file(path):
+    video_data = sp.check_output(
+        [
+            "ffprobe",
+            "-loglevel",
+            "error",
+            "-show_entries",
+            "format_tags",
+            "-of",
+            "json",
+            f"{path}",
+        ],
+        text=True,
+    )
+    if not video_data:
         log.error("Could not scrape video: ffprobe returned null")
         return
-    
+
     metadata = json.loads(video_data)["format"]["tags"]
-    metadata_insensitive = {}
+    metadata_insensitive = {key.lower(): metadata[key] for key in metadata}
 
-    for key in metadata:
-        metadata_insensitive[key.lower()] = metadata[key]
+    scene = {}
+    if title := metadata_insensitive.get("title"):
+        scene["title"] = title
 
-    if m_title := metadata_insensitive.get("title"):
-        scraped_metadata["title"] = m_title
+    if (comment := metadata_insensitive.get("comment")) and (url := parse_url(comment)):
+        scene["url"] = url
 
-    if m_comment := metadata_insensitive.get("comment"):
-        scraped_metadata["url"] = parse_url_from_comment(m_comment)
-    
-    if m_description := metadata_insensitive.get("description"):
-        scraped_metadata["details"] = m_description
+    if description := metadata_insensitive.get("description"):
+        scene["details"] = description
 
-    if m_date := metadata_insensitive.get("date"):
-        scraped_metadata["date"] = format_date(m_date)
+    if date := metadata_insensitive.get("date"):
+        scene["date"] = datetime.strptime(date, "%Y%m%d").strftime("%Y-%m-%d")
 
-    if m_artist := metadata_insensitive.get("artist"):
-        scraped_metadata["performers"] = [{"name": m_artist}]
-        scraped_metadata["studio"] = {"name": m_artist}
+    if date := metadata_insensitive.get("creation_time"):
+        scene["date"] = date[:10]
 
-    return scraped_metadata
+    if artist := metadata_insensitive.get("artist"):
+        scene["performers"] = [{"name": artist}]
+        scene["studio"] = {"name": artist}
 
-input = sys.stdin.read()
-js = json.loads(input)
+    return scene
 
-if sys.argv[1] == "metadata_from_primary":
-    ret = metadata_from_primary_path(js)
-    print(json.dumps(ret))
+
+if __name__ == "__main__":
+    fragment = json.loads(sys.stdin.read())
+    path = get_path(fragment["id"])
+    if not path:
+        print("null")
+        sys.exit(1)
+
+    scraped = scrape_file(path)
+    print(json.dumps(scraped))
