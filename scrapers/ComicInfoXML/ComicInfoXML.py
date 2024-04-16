5426229a10cb198e327f543c577d9073605ead61
Maista6969 <glad.beach5807@fastmail.com>
2024-04-16 05:35:00 +0200

    Update ComicInfoXML to handle XML files inside of zip/cbz files

--- scrapers/ComicInfoXML/ComicInfoXML.py
+++ scrapers/ComicInfoXML/ComicInfoXML.py
@@ -2,62 +2,71 @@ import json
 import pathlib
 import sys
 import xml.etree.ElementTree as ET
+import zipfile
 
 import py_common.graphql as graphql
 import py_common.log as log
 
 """
-This script parses xml files for metadata. 
-The .xml file must be in the same directory as the gallery files and named either ComicInfo.xml for loose files (folder full of jpg/png's) 
-or the same name as the .cbz/.zip file
+This script parses xml files for metadata
+
+If your gallery is in a .cbz/.zip file, the .xml file must either be:
+- the first .xml file inside of the .cbz/.zip
+- be in the same directory as the .cbz/.zip and have the same name
+
+If your gallery is a folder of loose files, the .xml file must be named ComicInfo.xml and be in the same directory as the gallery
 """
 
 
-def query_xml(gallery_path, title):
-    res = {"title": title}
+def query_xml(xml_file):
     try:
-        tree = ET.parse(gallery_path)
+        tree = ET.parse(xml_file)
     except Exception as e:
-        log.error(f"xml parsing failed:{e}")
+        log.error(f"Failed to parse XML file: {e}")
         print("null")
         exit(1)
 
-    if (node := tree.find("Title")) and (original_title := node.text):
-        res["title"] = original_title
+    scraped = {}
 
-    if (node := tree.find("Web")) and (url := node.text):
-        res["url"] = url
+    if (node := tree.find("Title")) is not None and (title := node.text):
+        scraped["title"] = title
 
-    if (node := tree.find("Summary")) and (details := node.text):
-        res["details"] = details
+    if (node := tree.find("Web")) is not None and (url := node.text):
+        scraped["url"] = url
 
-    if (node := tree.find("Released")) and (date := node.text):
-        res["date"] = date
+    if (node := tree.find("Summary")) is not None and (details := node.text):
+        scraped["details"] = details.strip()
+
+    if (node := tree.find("Released")) is not None and (date := node.text):
+        scraped["date"] = date
 
     year = month = day = None
-    if node := tree.find("Year"):
+    if (node := tree.find("Year")) is not None:
         year = node.text
-    if node := tree.find("Month"):
+    if (node := tree.find("Month")) is not None:
         month = node.text
-    if node := tree.find("Day"):
+    if (node := tree.find("Day")) is not None:
         day = node.text
 
     if year and month and day:
-        res["date"] = f"{year}-{month:>02}-{day:>02}"
+        scraped["date"] = f"{year}-{month:>02}-{day:>02}"
 
-    if (node := tree.find("Genre")) and (tags := node.text):
-        res["tags"] = [{"name": x} for x in tags.split(", ")]
+    if (node := tree.find("Genre")) is not None and (tags := node.text):
+        scraped["tags"] = [{"name": x} for x in tags.split(", ")]
 
-    if (node := tree.find("Series")) and (series := node.text):
-        res["tags"] = res.get("tags", []) + [{"name": f"Series/Parody: {series}"}]
+    # Stash has no concept of Series so we include it as a custom tag
+    if (node := tree.find("Series")) is not None and (series := node.text):
+        scraped["tags"] = scraped.get("tags", []) + [
+            {"name": f"Series/Parody: {series}"}
+        ]
 
-    if (node := tree.find("Characters")) and (characters := node.text):
-        res["performers"] = [{"name": x} for x in characters.split(", ")]
+    if (node := tree.find("Characters")) is not None and (characters := node.text):
+        scraped["performers"] = [{"name": x} for x in characters.split(", ")]
 
-    if (node := tree.find("Writer")) and (studio := node.text):
-        res["studio"] = {"name": studio}
+    if (node := tree.find("Writer")) is not None and (studio := node.text):
+        scraped["studio"] = {"name": studio}
 
-    return res
+    return scraped
 
 
 if sys.argv[1] == "query":
@@ -68,21 +77,27 @@ if sys.argv[1] == "query":
         sys.exit(1)
 
     p = pathlib.Path(gallery_path)
-    # Determine if loose file format or archive such as .cbz or .zip
-    if "cbz" in gallery_path or "zip" in gallery_path:
-        # Look for filename.xml where filename.(cbz|zip) is the gallery
-        f = p.with_suffix(".xml")
-        log.debug(f"Single File Format: trying '{f}'")
-    else:
-        # Use loose files format
-        # Look for ComicInfo.xml in the gallery's folder
-        f = p.resolve() / "ComicInfo.xml"
-        log.debug(f"Folder format: trying '{f}'")
+    f = None
 
-    if not f.is_file():
-        log.warning(f"No xml files found for the gallery: {p}")
+    log.debug(f"Searching for ComicInfo.xml based on gallery path: {p}")
+    if p.suffix in (".cbz", ".zip"):
+        log.debug("Gallery is an archive file")
+        # Look inside the archive for the xml file
+        archive = zipfile.ZipFile(p)
+        if xmlfile := next((x for x in archive.namelist() if x.endswith(".xml")), None):
+            log.debug(f"Found '{xmlfile}' inside '{archive.filename}'")
+            f = archive.open(xmlfile)
+    elif p.is_dir() and (xmlfile := p.resolve() / "ComicInfo.xml") and xmlfile.exists():
+        log.debug(f"Found '{xmlfile}' in '{p}'")
+        f = xmlfile.open()
+    elif (xmlfile := p.with_suffix(".xml")) and xmlfile.exists():
+        log.debug(f"Found '{xmlfile}' in the same directory as '{p}'")
+        f = xmlfile.open()
+
+    if not f:
+        log.warning(f"No XML files found for the gallery: {p}")
         print("null")
-        sys.exit(1)
+        sys.exit()
 
-    res = query_xml(f, fragment["title"])
+    res = query_xml(f)
     print(json.dumps(res))
