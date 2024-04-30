385468bc7a4a6dbe8b0d7330682812123b6111c1
MortonBridges <97797486+MortonBridges@users.noreply.github.com>
2024-04-30 19:13:20 -0400

    New sites and performer scrapers for KBProductions (#1798)
    * Update KBProductions.yml
    
    Added sites to scene and performer scrapers:
    jizzaddiction.com
    joeschmoevideos.com
    machofactory.com
    
    * Update KBProductions.py
    
    Modified performer scraper:
    - Forcing keys for raw_performer dict to lowercase, since some new sites have performer data with lower case keys.
    - Added additional performer values for penis size, circumcised, and some tags.
    - Added some utility routines to convert imperial measures to metric.
    - Added a "site uses imperial" list for those that have unit-less measures but are obviously in imperial (size 8 penis, for example, when the performer picture clearly shows more than 8cm...)
    
    Added studio map entries:
    jizzaddiction.com
    joeschmoevideos.com
    machofactory.com
    
    * Update SCRAPERS-LIST.md
    
    Added studio entries that use KBProductions.yml:
    jizzaddiction.com
    joeschmoevideos.com
    machofactory.com
    
    * Update SCRAPERS-LIST.md
    
    Tagged new sites as "gay" in the notes column.
    
    * Update scrapers/KBProductions/KBProductions.py
    
    Co-authored-by: Maista <131594657+Maista6969@users.noreply.github.com>
    
    * Update scrapers/KBProductions/KBProductions.py
    
    Co-authored-by: Maista <131594657+Maista6969@users.noreply.github.com>
    
    * Update scrapers/KBProductions/KBProductions.py
    
    Co-authored-by: Maista <131594657+Maista6969@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Maista <131594657+Maista6969@users.noreply.github.com>

--- scrapers/KBProductions/KBProductions.py
+++ scrapers/KBProductions/KBProductions.py
@@ -39,10 +39,13 @@ studio_map = {
     "inkedpov.com": "Inked POV",
     "inserted.com": "Inserted",
     "jav888.com": "JAV888",
+    "jizzaddiction.com": "Jizz Addiction",
+    "joeschmoevideos.com": "Joe Schmoe Videos",
     "lady-sonia.com": "Lady Sonia",
     "legendaryx.com": "Legendary X",
     "lezkey.com": "LezKey",
     "lucidflix.com": "LucidFlix",
+    "machofactory.com": "Macho Factory",
     "meanfeetfetish.com": "Mean Feet Fetish",
     "members.hobybuchanon.com": "Hoby Buchanon",
     "mongerinasia.com": "Monger In Asia",
@@ -161,53 +164,105 @@ def get_studio(site: str) -> ScrapedStudio:
 
 
 def to_scraped_performer(raw_performer: dict) -> ScrapedPerformer:
+    # Convert dict keys to lower case because, of couse, they can come in differently depending on studio.
+    raw_performer = {key.lower():value for key,value in raw_performer.items()}
+    
+    # Studios that do not use units for measurements, but are obviously not metric.
+    STUDIO_USES_IMPERIAL = [
+        "joeschmoevideos.com",
+        "jizzaddiction.com",
+    ]
+
     performer: ScrapedPerformer = {
         "name": raw_performer["name"],
         "gender": raw_performer["gender"],
         "url": make_performer_url(raw_performer["slug"], raw_performer["site_domain"]),
+        "tags": [],
     }
 
     if image := raw_performer.get("thumb"):
         performer["image"] = image
+    elif image := raw_performer.get("thumbnail"):
+        image = re.sub(r'^//','https://',image)
+        performer["image"] = image
 
-    if bio := raw_performer.get("Bio"):
+    if bio := raw_performer.get("bio"):
         performer["details"] = strip_tags(bio)
 
-    if (birthdate := raw_performer.get("Birthdate")) and birthdate != "1969-12-31":
+    if (birthdate := raw_performer.get("birthdate")) and birthdate != "1969-12-31":
         performer["birthdate"] = birthdate
 
-    if measurements := raw_performer.get("Measurements"):
+    if measurements := raw_performer.get("measurements"):
         performer["measurements"] = measurements
 
-    if eye_color := raw_performer.get("Eyes"):
+    if eye_color := raw_performer.get("eyes"):
         performer["eye_color"] = eye_color
 
-    if ethnicity := raw_performer.get("Ethnicity"):
+    if ethnicity := raw_performer.get("ethnicity"):
+        performer["ethnicity"] = ethnicity
+    elif ethnicity := raw_performer.get("race"):
         performer["ethnicity"] = ethnicity
 
-    if (height_ft := raw_performer.get("Height")) and (
+    if (height_ft := raw_performer.get("height")) and (
         h := re.match(r"(\d+)\D+(\d+).+", height_ft)
     ):
-        height_cm = round((float(h.group(1)) * 12 + float(h.group(2))) * 2.54)
+        height_cm = feetinches_to_cm(h.group(1),h.group(2))
+        performer["height"] = str(height_cm)
+    elif (height_m := raw_performer.get("height")) and (
+        h := re.match(r"^(\d\.\d\d)$", height_m)
+    ):
+        height_cm = float(h.group(1)) * 100
         performer["height"] = str(height_cm)
 
-    if (weight_lb := raw_performer.get("Weight")) and (
+    elif (height_cm := raw_performer.get("height")) and (
+        h := re.match(r"^(\d)+$", height_cm)
+    ):
+        performer["height"] = str(height_cm)
+
+    if (weight_lb := raw_performer.get("weight")) and (
         w := re.match(r"(\d+)\slbs", weight_lb)
     ):
         weight_kg = round(float(w.group(1)) / 2.2046)
         performer["weight"] = str(weight_kg)
+    elif (weight_kg := raw_performer.get("weight")) and (
+        w := re.match(r"(\d+)\skg", weight_kg)
+    ):
+        performer["weight"] = str(w.group(1))
+    elif (weight_nounits := raw_performer.get("weight")) and (
+        w := re.match(r"^([\d\.]+)$", weight_nounits)
+    ):
+        performer["weight"] = lbs_to_kg(w.group(1)) if raw_performer["site_domain"] in STUDIO_USES_IMPERIAL else str(w.group(1))
 
-    if hair_color := raw_performer.get("Hair"):
+    if (penis_nounits:= raw_performer.get("dick size")) and (
+        s := re.match(r"^([\d\.]+)$", penis_nounits)
+    ):
+        performer["penis_length"] = feetinches_to_cm(0,s.group(1)) if raw_performer["site_domain"] in STUDIO_USES_IMPERIAL else str(s.group(1))
+
+    if circumcised := raw_performer.get("cut / uncut"):
+        performer["circumcised"] = circumcised.capitalize()
+
+    if hair_color := raw_performer.get("hair"):
         performer["hair_color"] = hair_color
 
-    if country := raw_performer.get("Born"):
+    if country := raw_performer.get("born"):
         performer["country"] = guess_nationality(country)
 
-    if twitter := raw_performer.get("Twitter", "").removeprefix("@"):
+    if twitter := raw_performer.get("wwitter", "").removeprefix("@"):
         performer["twitter"] = f"https://twitter.com/{twitter}"
 
-    if instagram := raw_performer.get("Instagram", "").removeprefix("@"):
+    if instagram := raw_performer.get("instagram", "").removeprefix("@"):
         performer["instagram"] = f"https://www.instagram.com/{instagram}"
+
+    if "orientation" in raw_performer:
+        performer["tags"].append({"name": raw_performer["orientation"]})
+    if "sexual positions" in raw_performer:
+        log.debug("positions!")
+        for x in raw_performer["sexual positions"].split(" "):
+            performer["tags"].append({"name": x})
+    if "body" in raw_performer:
+        performer["tags"].append({"name": raw_performer["body"]})
+    if "pubic" in raw_performer:
+        performer["tags"].append({"name": raw_performer["pubic"] + " Pubes"})
 
     return performer
 
@@ -342,6 +397,14 @@ def scrape_performer(url: str) -> ScrapedPerformer | None:
         return None
 
     return to_scraped_performer(props["model"])
+
+
+def feetinches_to_cm(feet,inches):
+    return(str(round((float(feet) * 12 + float(inches)) * 2.54)))
+
+
+def lbs_to_kg(lbs):
+    return(str(round(float(lbs) / 2.2046)))
 
 
 if __name__ == "__main__":
