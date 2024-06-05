9c0ff1d7b3f842245bc80f1c75d60e693a1a8295
feederbox826 <feederbox826@users.noreply.github.com>
2024-06-05 15:12:26 -0400

    add compatibility with old API format
    thanks to discord user for helping clarify bellesa

--- scrapers/BellesaPlus/BellesaPlus.py
+++ scrapers/BellesaPlus/BellesaPlus.py
@@ -5,6 +5,7 @@ import sys
 from py_common import log
 from datetime import datetime
 
+# Replace bellesa tags with stash tags (make sure hyphen is preserved)
 tag_replacements = {
     "Penetration": "Vaginal Penetration",
     "Dick-Riding": "Riding",
@@ -12,51 +13,38 @@ tag_replacements = {
     "Sex": "Hardcore",
     "Lesbian-Rough": "Rough",
     "Unscripted": "Gonzo",
-    "Partner Swapping": "Swapping",
+    "Partner-Swapping": "Swapping",
     "Masturbating": "Masturbation"
 }
+# tags that do not have a stash equivalent or are invalid either way
+# make sure hyphens are preserved
 bad_tags = [
     "original",
-    "hot guy",
-    "hot girls",
+    "hot-guy",
+    "hot-girls",
     "bellesa",
-    "bellesa houses"
+    "bellesa-houses"
 ]
 
-def scrape_scene(url):
-    # replace URL with api url
-    videoIDmatch = re.search(r'(\/videos\/)(\d+)(\/.+)', url)
-    if videoIDmatch is None:
-        log.error("Invalid URL")
-        sys.exit(1)
-    videoID = videoIDmatch.group(2)
-    api_url = f"https://www.bellesa.co/api/rest/v1/videos/{videoID}"
-    response = requests.get(api_url)
-    data = response.json()
-    # check if response is from bellesaplus
-    if (data.get("reference") != "plusSubscriptionRequired"):
-        log.error("This video URL is from bellesa.co and is a re-release.")
-        print("{}")
-        sys.exit(1)
-    body = data["value"]
-    # craft dictionary response to stash
+def parse_response(data):
     res = {}
-    res["title"] = body["title"]
-    res["details"] = body["description"]
-    res["image"] = body["image"]
-    res["code"] = str(body["id"])
+    res["title"] = data["title"]
+    res["details"] = data["description"]
+    res["image"] = data["image"]
+    res["code"] = str(data["id"])
     res["studio"] = {}
-    res["studio"]["name"] = body["content_provider"][0]["name"]
+    res["studio"]["name"] = data["content_provider"][0]["name"]
     res["performers"] = []
-    for performer in body["performers"]:
+    for performer in data["performers"]:
         res["performers"].append({"name": performer["name"]})
     res["tags"] = []
     # clean tags
-    temptags = body["tags"].split(",")
+    temptags = data["tags"].split(",")
     # remove studio
     studioTag = res["studio"]["name"].lower()
-    # create badtags array - studio name, performers
+    # remove studio from tags
     bad_tags.append(studioTag)
+    # remove performers from tags
     for performer in res["performers"]:
         bad_tags.append(performer["name"].lower())
     for tag in temptags:
@@ -69,8 +57,33 @@ def scrape_scene(url):
             tag = tag.replace("-", " ")
             res["tags"].append({"name": tag})
     # Date
-    res["date"] = datetime.fromtimestamp(body["posted_on"]).strftime('%Y-%m-%d')
+    date = datetime.fromtimestamp(data["posted_on"]).strftime('%Y-%m-%d')
+    res["date"] = date
     print(json.dumps(res))
+
+def scrape_scene(url):
+    # replace URL with api url
+    videoIDmatch = re.search(r'(\/videos\/)(\d+)(\/.+)', url)
+    if videoIDmatch is None:
+        log.error("Invalid URL")
+        sys.exit(1)
+    videoID = videoIDmatch.group(2)
+    api_url = f"https://www.bellesa.co/api/rest/v1/videos/{videoID}"
+    response = requests.get(api_url)
+    data = response.json()
+    # check if response is nested
+    if data.get("value") is not None:
+        # if nested, proceed to nested data
+        data = data["value"]
+    # if not nested, proceed normally
+
+    # check if response is from bellesaplus
+    # bellesa scenes also include free redistributions and delayed scenes
+    if data["plus"] != 1 or data["bellesa"] == 1:
+        log.error("This video URL is from bellesa.co (free) and not bellesaplus.co (premium)")
+        print("{}")
+        sys.exit(1)
+    parse_response(data)
 
 def main():
     fragment = json.loads(sys.stdin.read())
