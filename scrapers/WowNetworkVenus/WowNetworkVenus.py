ba2bf07a0dc6ff0a22cbf530668661050c345324
Maista6969 <glad.beach5807@fastmail.com>
2024-07-06 21:56:38 +0200

    Update Wow Network scraper
    Remove old-style import guards
    Replace bare exceptions with Exception so it can be logged

--- scrapers/WowNetworkVenus/WowNetworkVenus.py
+++ scrapers/WowNetworkVenus/WowNetworkVenus.py
@@ -1,81 +1,45 @@
-import base64
 import json
-import os
 import re
 import sys
 import urllib.parse
 from itertools import chain, zip_longest
 
-# to import from a parent directory we need to add that directory to the system path
-csd = os.path.dirname(
-    os.path.realpath(__file__))  # get current script directory
-parent = os.path.dirname(csd)  #  parent directory (should be the scrapers one)
-sys.path.append(
-    parent
-)  # add parent dir to sys path so that we can import py_common from there
+import py_common.log as log
+import requests
 
-try:
-    import py_common.log as log
-except ModuleNotFoundError:
-    print(
-        "You need to download the folder 'py_common' from the community repo! (CommunityScrapers/tree/master/scrapers/py_common)",
-        file=sys.stderr,
-    )
-    sys.exit()
-try:
-    import requests
-except ModuleNotFoundError:
-    log.error(
-        "You need to install the requests module. (https://docs.python-requests.org/en/latest/user/install/)"
-    )
-    log.error(
-        "If you have pip (normally installed with python), run this command in a terminal (cmd): pip install requests"
-    )
-    sys.exit()
+from lxml import html
 
-try:
-    from lxml import etree, html
-except ModuleNotFoundError:
-    log.error(
-        "You need to install the lxml module. (https://lxml.de/installation.html#installation)"
-    )
-    log.error(
-        "If you have pip (normally installed with python), run this command in a terminal (cmd): pip install lxml"
-    )
-    sys.exit()
-
-
-STUDIOS = {"Ultra Films": None, "All Fine Girls": 24,
-           "WowGirls": 32, "WowPorn": 36}
 PROXIES = {}
 TIMEOUT = 10
+
+STUDIOS = {
+    "Ultra Films": None,
+    "All Fine Girls": 24,
+    "WowGirls": 32,
+    "WowPorn": 36,
+}
+
+session = requests.Session()
+session.proxies.update(PROXIES)
 
 
 class WowVenus:
     def __init__(self):
-        self.session = requests.Session()
-        self.session.headers.update(
-            {"content-type": "application/x-www-form-urlencoded; charset=UTF-8"}
-        )
-        self.session.proxies.update(PROXIES)
         self.search_results = {}
 
     def count_results_pages(self, studio_name):
-        try:
-            return len(self.search_results.get(studio_name))
-        except:
-            return 0
+        return len(self.search_results.get(studio_name, []))
 
     def wow_sub_studio_filter_toggle(self, studio_key, studio_name):
         query_studio_name = studio_name.replace(" ", "").lower()
         data = f"__operation=toggle&__state=sites%3D{studio_key}"
         scraped = None
         try:
-            scraped = self.session.post(
+            scraped = session.post(
                 f"https://venus.{query_studio_name}.com/search/cf", data=data
             )
-        except:
-            log.error("scrape error")
+        except Exception as ex:
+            log.error(f"scrape error: {ex}")
             return None
         if scraped.status_code >= 400:
             log.error(f"HTTP Error: {scraped.status_code}")
@@ -91,16 +55,15 @@ class WowVenus:
                 "//div[@class='paginator']/div[@class='pages']//text()"
             )
             for pageNu in pagignator[1:]:
-                page_content = html.fromstring(
-                    self.pageNu_scrape(studio_name, pageNu))
+                page_content = html.fromstring(self.pageNu_scrape(studio_name, pageNu))
                 self.search_results[studio_name].append(page_content)
 
     def GET_req(self, url):
         scraped = None
         try:
-            scraped = self.session.get(url, timeout=TIMEOUT)
-        except:
-            log.error("scrape error")
+            scraped = session.get(url, timeout=TIMEOUT)
+        except Exception as ex:
+            log.error(f"scrape error: {ex}")
             return None
         if scraped.status_code >= 400:
             log.error(f"HTTP Error: {scraped.status_code}")
@@ -113,9 +76,9 @@ class WowVenus:
         data = "__state=contentTypes%3D%5Bvideo%5D"
         scraped = None
         try:
-            scraped = self.session.post(url, data=data, timeout=TIMEOUT)
-        except:
-            log.error("scrape error")
+            scraped = session.post(url, data=data, timeout=TIMEOUT)
+        except Exception as ex:
+            log.error(f"scrape error: {ex}")
             return None
         if scraped.status_code >= 400:
             log.error(f"HTTP Error: {scraped.status_code}")
@@ -128,20 +91,20 @@ class WowVenus:
         url = f"https://venus.{query_studio_name}.com/search/cf"
         data = f"__state=paginator.page%3D{pageNu}"
         try:
-            scraped = self.session.post(url, data=data, timeout=TIMEOUT)
-        except:
-            log.error("scrape error")
+            scraped = session.post(url, data=data, timeout=TIMEOUT)
+        except Exception as ex:
+            log.error(f"scrape error: {ex}")
         if scraped.status_code >= 400:
             log.error(f"HTTP Error: {scraped.status_code}")
         scraped = scraped.content.decode("utf-8")
         return scraped
 
-    def output_json(self, title, tags, url, b64img, studio, performers):
+    def output_json(self, title, tags, url, img, studio, performers):
         return {
             "title": title,
             "tags": [{"name": x} for x in tags],
             "url": url,
-            "image": "data:image/jpeg;base64," + b64img.decode("utf-8"),
+            "image": img,
             "studio": {"name": studio},
             "performers": [{"name": x.strip()} for x in performers],
         }
@@ -150,12 +113,10 @@ class WowVenus:
         title = scene_card.xpath('./a[@class="title"]/text()')[0].strip()
         imgurl = scene_card.xpath(".//img[@title]/@src")[0]
         if URL:
-            imgurl = re.sub("_\w*", "_1280x720", imgurl)
-        img = self.GET_req(imgurl)
-        b64img = base64.b64encode(img)
+            imgurl = re.sub(r"_\w*", "_1280x720", imgurl)
         performers = scene_card.xpath('.//*[@class="models"]/a/text()')
         tags = scene_card.xpath('.//span[@class="genres"]/a/text()')
-        return title, b64img, performers, tags
+        return title, imgurl, performers, tags
 
     def parse_results(self):  # parse all scene elements, return all
         parsed_scenes = {}
@@ -170,13 +131,11 @@ class WowVenus:
                         f"https://venus.{query_studio_name}.com"
                         + scene_card.xpath("./a/@href")[0]
                     )
-                    title, b64img, performers, tags = self.scene_card_parse(
-                        scene_card)
+                    title, img, performers, tags = self.scene_card_parse(scene_card)
                     if not parsed_scenes.get(query_studio_name):
                         parsed_scenes[query_studio_name] = []
                     parsed_scenes[query_studio_name].append(
-                        self.output_json(title, tags, url,
-                                         b64img, studio_name, performers)
+                        self.output_json(title, tags, url, img, studio_name, performers)
                     )
         return parsed_scenes
 
@@ -195,8 +154,7 @@ class WowVenus:
                         f"https://venus.{query_studio_name}.com"
                         + scene_card.xpath("./a/@href")[0]
                     )
-                    title, b64img, performers, tags = self.scene_card_parse(
-                        scene_card)
+                    title, b64img, performers, tags = self.scene_card_parse(scene_card)
                     return self.output_json(
                         title, tags, url, b64img, studio_name, performers
                     )
@@ -204,18 +162,20 @@ class WowVenus:
     def search(self, query_title, studio_name, studio_key):
         query_studio_name = studio_name.replace(" ", "").lower()
         url = f"https://venus.{query_studio_name}.com/search/?query={query_title}"
-        self.GET_req(url)   # send search request, needed for session data
+        self.GET_req(url)  # send search request, needed for session data
         # set 'video only' filter for results
         scraped = self.set_video_filter(query_studio_name)
         page_content = html.fromstring(scraped)
         if studio_key:  # use studio_key to filter search results by sub studio
             scraped = self.wow_sub_studio_filter_toggle(
-                studio_key, query_studio_name)  # toggle on
+                studio_key, query_studio_name
+            )  # toggle on
             page_content = html.fromstring(scraped)
         self.scrape_all_results_pages(page_content, studio_name)
         if studio_key:
             self.wow_sub_studio_filter_toggle(
-                studio_key, query_studio_name)  # toggle off
+                studio_key, query_studio_name
+            )  # toggle off
         log.debug(
             f"Searched {studio_name}, found {self.count_results_pages(studio_name)} pages"
         )
