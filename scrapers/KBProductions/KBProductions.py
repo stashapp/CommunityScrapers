28cbedea41b95822dc378dae1688e9f4e31f3d8e
feederbox826 <144178721+feederbox826@users.noreply.github.com>
2024-08-30 17:06:13 -0400

    [KBProductions] add high-res trailer.jpg (#2004)
    Co-authored-by: feederbox826 <feederbox826@users.noreply.github.com>
    Co-authored-by: DogmaDragon <103123951+DogmaDragon@users.noreply.github.com>

--- scrapers/KBProductions/KBProductions.py
+++ scrapers/KBProductions/KBProductions.py
@@ -317,6 +317,7 @@ def to_scraped_scene_from_content(raw_scene: dict) -> ScrapedScene:
 
     scene["studio"] = get_studio(site)
 
+    # trailer seems to give the best quality image (2024/08/28)
     # trailer_screencap is what's shown on most sites
     # extra_thumbnails has the best sizes and in most cases the first one is the same as thumb
     # thumb is a good fallback if extra_thumbnails is not available
@@ -324,6 +325,7 @@ def to_scraped_scene_from_content(raw_scene: dict) -> ScrapedScene:
     cover_candidates = filter(
         None,
         (
+            dig(raw_scene, "trailer"),
             dig(raw_scene, "poster_url"),
             dig(raw_scene, "trailer_screencap"),
             dig(raw_scene, "extra_thumbnails", 0),
