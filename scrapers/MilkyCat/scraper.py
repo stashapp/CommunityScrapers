626ad0ce3d5759cd83fca2505ef6ea5365ff13da
0x60B2 <>
2024-09-23 21:28:42 +0200

    feat(MilkyCat): Support dates via the website news panel

--- scrapers/MilkyCat/scraper.py
+++ scrapers/MilkyCat/scraper.py
@@ -1,15 +1,56 @@
 import json
 import sys
 import string, re
-import bs4, requests
+import bs4, requests  
 from os import path
+import datetime
+import urllib.parse
+from py_common.cache import cache_to_disk
 
-def readJSONInput():
+@cache_to_disk("milkycat_news", 24 * 60 * 60)
+def fetchNews():
+  news_url = "https://www.milky-cat.com/movie/news.html"
+  content = requests.get(news_url).text
+  soup = bs4.BeautifulSoup(content, 'html.parser')
+  lines = [[l.strip() for l in s.split("\n")] for s in soup.select_one("td").text.split("\n\n")]
+  # Look for the first news item that looks like:
+  # 2013/7/12 add movie.
+  # 2007/9/10 add 2 movies.
+  news = []
+  for line in lines:
+    e = re.match(r"^([0-9]{4}\/[0-9]{1,2}\/[0-9]{1,2}) add (?:\d+ movies|movie).$", line[0])
+    if not e:
+      continue
+    date = e.group(1)
+    year, month, day = map(int, date.split("/"))
+    news_date = datetime.date(year, month, day)
+    if news_date > datetime.date.today():
+      continue
+    for line in line[1:]:
+      # if we can match the regex for "<long title> (<studio_code>)", then we have a match
+      e = re.match(r"^- ([^(]+)\(([a-zA-Z0-9]{2,4}\s?-\s?[a-zA-Z0-9]{2,4})\)$", line)
+      if e:
+        news.append({
+          "Title": e.group(1),
+          "Code": e.group(2),
+          "Date": news_date.strftime("%Y-%m-%d"),
+        })
+  return news
+
+def readJSONInput(): 
 	input = sys.stdin.read()
 	return json.loads(input)
 
 def debugPrint(t):
     sys.stderr.write(t + "\n")
+
+def prettyCodeFromURL(url):
+  query = urllib.parse.urlparse(url).query
+  # we get something like 'blt04'
+  m = re.match(r"^([a-z]{2,4})(\d{1,4})$", query)
+  if m:
+    return f"{m.group(1)}-{m.group(2)}".upper()
+  return None
 
 def sceneByURL(url):
   # debugPrint(f"sceneByURL({url})")
@@ -22,15 +63,27 @@ def sceneByURL(url):
   jackett = soup.select_one(".jacket")
   image_url = jackett["href"] if jackett else None
   image = image_url if requests.head(image_url).status_code == 200 else None
-  return [{
+  pretty_code = prettyCodeFromURL(url)
+  # debugPrint(f"pretty_code: {pretty_code}")
+  news = fetchNews()
+  # debugPrint(f"news: {news}")
+  matching_news = next(filter(lambda x: x["Code"] == pretty_code, news), None)
+  out = {
     "Title": title,
     "Details": desc,
     "Image": image,
     "Url": url,
     # "category": category,
     # "tags": [e[0] for e in table if e[0] != "category"],
-  }]
+  }
+  if matching_news:
+    # format in Go format: 2006-01-02
+    out["Date"] = matching_news["Date"]
+  else:
+    # debugPrint(f"No matching news for {pretty_code}")
+    pass
 
+  return [out]
 def sceneByName(name):
   # debugPrint(f"sceneByName({name})")
   # remove all spaces and anyting not in [a-zA-Z0-9] after the regex /[a-zA-Z]{2,4}\s?-\s?[0-9]{1,4}/
@@ -84,6 +137,8 @@ def getFirstOrNone(l):
     return l[0]
   except IndexError:
     return None
+
+
 
 # read the input 
 i = readJSONInput()
