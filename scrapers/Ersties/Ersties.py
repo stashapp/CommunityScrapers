f7d485d833bdfef6b01826c3f66f0773b78d6c64
shark_lasers58 <shark_lasers58@protonmail.com>
2024-10-21 12:14:16 -0400

    Added Movie/Group scraper

--- scrapers/Ersties/Ersties.py
+++ scrapers/Ersties/Ersties.py
@@ -59,7 +59,40 @@ def get_scene(inputurl):
 
     return ret
 
-if sys.argv[1] == 'scrapeByURL':
+def get_group(inputurl):
+    # Use a regular expression to extract the number after 'profile/'
+    match = re.search(r'profile/(\d+)', inputurl)
+
+    # Check if the pattern was found and save it as a variable
+    if match:
+        groupid = match.group(1)  
+    else:
+        debugPrint('No match found')
+
+    #Build URL to scrape group
+    scrape_url='https://api.ersties.com/models/'+groupid
+
+    #Scrape URL
+    scrape = requests.get(scrape_url, headers=scrape_headers)
+
+    #Parse response
+    scrape_data = scrape.json()
+
+    ret = {}
+
+    ret['name'] = scrape_data['name_en']
+    ret['synopsis'] = scrape_data['description_en']
+    ret['studio'] = {'name':'Ersties'}
+    ret['front_image'] = f'https://thumb.ersties.com/width=510,height=660,fit=cover,quality=85,sharpen=1,format=jpeg/content/images_mysql/Model_Cover_Image/backup/'+scrape_data['thumbnail']  
+
+    return ret
+
+if sys.argv[1] == 'sceneByURL':
     i = readJSONInput()
     ret = get_scene(i.get('url'))
+    print(json.dumps(ret))
+
+if sys.argv[1] == 'groupByURL':
+    i = readJSONInput()
+    ret = get_group(i.get('url'))
     print(json.dumps(ret))
