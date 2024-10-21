5e44d0f2a04daa5977014e42ceb4232121edcdf3
shark_lasers58 <shark_lasers58@protonmail.com>
2024-10-20 21:58:42 -0400

    Reformated code for future expanability

--- scrapers/Ersties/Ersties.py
+++ scrapers/Ersties/Ersties.py
@@ -20,43 +20,46 @@ def readJSONInput():
     input = sys.stdin.read()
     return json.loads(input)
 
-#Read the input
-i = readJSONInput()
-sys.stderr.write(json.dumps(i))
+def debugPrint(t):
+    sys.stderr.write(t + "\n")
 
-#Scrape Scene URL
+def get_scene(inputurl):
 
-#Get Scene ID from url
-inputurl= i['url']
-# Use a regular expression to extract the number after '#play-' and before '-comments'
-match = re.search(r'#play-(\d+)-comments', inputurl)
-# Check if the pattern was found and save it as a variable
-if match:
-    sceneid = match.group(1)  # The captured group (7976)
-else:
-    print('No match found')
+    # Use a regular expression to extract the number after '#play-' and before '-comments'
+    match = re.search(r'#play-(\d+)-comments', inputurl)
 
-#Build URL to scrape
-scrape_url='https://api.ersties.com/videos/'+sceneid
+    # Check if the pattern was found and save it as a variable
+    if match:
+        sceneid = match.group(1)  
+    else:
+        debugPrint('No match found')
 
-#Scrape URL
-scrape = requests.get(scrape_url, headers=scrape_headers)
+    #Build URL to scrape
+    scrape_url='https://api.ersties.com/videos/'+sceneid
 
-#Parse response
+    #Scrape URL
+    scrape = requests.get(scrape_url, headers=scrape_headers)
 
-scrape_data = scrape.json()
+    #Parse response
 
-ret = {}
+    scrape_data = scrape.json()
 
-ret['title'] = scrape_data['title_en']
-ret['code'] = str(scrape_data['id'])
-ret['details'] = scrape_data['model']['description_en']
-ret['studio'] = {'name':'Ersties'}
-ret['tags'] = [{'name': x['name_en']} for x in scrape_data['tags']]
-ret['performers'] = [{'name': x['name_en']} for x in scrape_data['participated_models']]
-for thumbnail in scrape_data['thumbnails']:
-    if thumbnail['is_main']:
-        ret['image'] = f'https://thumb.ersties.com/width=900,height=500,fit=cover,quality=85,sharpen=1,format=avif/content/images_mysql/images_videothumbnails/backup/'+thumbnail['file_name']
-        break
+    ret = {}
 
-print(json.dumps(ret))
+    ret['title'] = scrape_data['title_en']
+    ret['code'] = str(scrape_data['id'])
+    ret['details'] = scrape_data['model']['description_en']
+    ret['studio'] = {'name':'Ersties'}
+    ret['tags'] = [{'name': x['name_en']} for x in scrape_data['tags']]
+    ret['performers'] = [{'name': x['name_en']} for x in scrape_data['participated_models']]
+    for thumbnail in scrape_data['thumbnails']:
+        if thumbnail['is_main']:
+            ret['image'] = f'https://thumb.ersties.com/width=900,height=500,fit=cover,quality=85,sharpen=1,format=avif/content/images_mysql/images_videothumbnails/backup/'+thumbnail['file_name']
+            break
+
+    return ret
+
+if sys.argv[1] == 'scrapeByURL':
+    i = readJSONInput()
+    ret = get_scene(i.get('url'))
+    print(json.dumps(ret))
