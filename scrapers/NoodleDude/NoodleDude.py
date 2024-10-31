c8c05910773e43272ac960d34b2b79df44136626
S3L3CT3DLoves <153332410+S3L3CT3DLoves@users.noreply.github.com>
2024-10-31 22:38:26 +0100

    Add performerUrl scraper

--- scrapers/NoodleDude/NoodleDude.py
+++ scrapers/NoodleDude/NoodleDude.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime
 import json
 import os
@@ -23,6 +24,12 @@ def base64_image(url) -> str:
     b64img_bytes = base64.b64encode(scraper.get(url).content)
     return f"data:image/jpeg;base64,{b64img_bytes.decode('utf-8')}"
 
+def xpath_string(tree, selector):
+    raw = tree.xpath(selector)
+    if not raw or len(raw) < 1:
+        return ""
+    return raw[0].strip()
+
 def scrape(url: str, retries=0):
     try:
         scraped = scraper.get(url, timeout=(3, 7))
@@ -39,10 +46,7 @@ def scrape(url: str, retries=0):
 
 
 def scene_title(tree):
-    raw = tree.xpath("//meta[@property='og:title']/@content")
-    if not raw or len(raw) < 1:
-        return ""
-    return raw[0].split('|')[0].strip()
+    return xpath_string(tree, "//meta[@property='og:title']/@content")
 
 def scene_date(tree):
     stash_date = "%Y-%m-%d"
@@ -69,23 +73,50 @@ def scene_tags(tree):
     # Tags do not appear anymore on the site
     return []
 
+def parse_performer_card(tree):
+    performer = {}
+    # Disable image - Too slow when there are 50+ performers in the list
+    # imgUrl = tree.xpath("img/@src")
+    # if imgUrl and len(imgUrl) == 1 and imgUrl[0] != "/static/images/placeholder.svg":
+    #     performer["images"] = [base64_image(imgUrl[0])]
+    performer["name"] = tree.xpath("div/span[1]/text()")[0]
+
+    performer["urls"] = ["https://www.noodledude.io" + tree.xpath("@href")[0]]
+    return performer
+
 def get_performers(tree):
     detailsUrl = tree.xpath("//*[@id='current-video']//button[contains(@class, 'btn-plus')]/@hx-get")
     if not detailsUrl or len(detailsUrl) < 1:
         return []
     
     scrapedPerformers = scrape("https://www.noodledude.io" + detailsUrl[0])
-    performerList = scrapedPerformers.xpath("//a[contains(@class, 'performer_card')]//span[1]/text()")
-    return [
-        {
-            "name" : performerName
-        }
-        for performerName in performerList
-    ]
+    performerNodes = scrapedPerformers.xpath("//a[contains(@class, 'performer_card')]")
+    performerList = map(parse_performer_card, performerNodes)
+    return list(performerList)
 
 def scene_image(tree):
     rawUrl = tree.xpath("//video[@id='player']/@poster")[0]
     return base64_image(rawUrl)
+
+
+def performer_name(tree):
+    return xpath_string(tree, "h1/text()")
+
+def performer_birthdate(tree):
+    return xpath_string(tree, "span[not(@class)]/span[@class='fc2']/@title")
+
+def performer_aliases(tree):
+    raw = tree.xpath("span[@class='fs-s']/text()")
+    if not raw or len(raw) == 0:
+        return ""
+    return ",".join(raw)
+
+def performer_urls(tree):
+    raw = tree.xpath("div[@class='performer-links']/a/@href")
+    if not raw or len(raw) == 0:
+        return []
+    return raw
+
 
 def scene_from_tree(tree):
     return {
@@ -100,10 +131,64 @@ def scene_from_tree(tree):
         "performers": get_performers(tree)
     }
 
-if __name__ == "__main__":
-    params = json.loads(sys.stdin.read())
-    scraped = scrape(params["url"])
+def performer_from_tree(tree):
+    performerTree = tree.xpath("//div[contains(@class, 'performer-main-info')]")[0]
+    return {
+        "name": performer_name(tree),
+        "urls": performer_urls(performerTree),
+        "birthdate": performer_birthdate(performerTree),
+        "aliases": performer_aliases(performerTree),
+        "images": [
+            base64_image(url) for url in tree.xpath("//img[@class='performer-image']/@src")
+        ],
+    }
+
+
+def main():
+    parser = argparse.ArgumentParser("NoodleDude Scraper", argument_default="")
+    subparsers = parser.add_subparsers(
+        dest="operation", help="Operation to perform", required=True
+    )
+
+    subparsers.add_parser("performer", help="Scrape a performer").add_argument(
+        "url", nargs="?", help="Performer URL"
+    )
+    subparsers.add_parser("scene", help="Scrape a scene").add_argument(
+        "url", nargs="?", help="Scene URL"
+    )
+
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    args = parser.parse_args()
+
+    # Script is being piped into, probably by Stash
+    if not sys.stdin.isatty():
+        try:
+            frag = json.load(sys.stdin)
+            args.__dict__.update(frag)
+            log.debug(f"With arguments from stdin: {args}")
+        except json.decoder.JSONDecodeError:
+            log.error("Received invalid JSON from stdin")
+            sys.exit(1)
+
+    url = args.url
+    if not url:
+        log.error("No URL provided")
+        sys.exit(1)
+    
+    log.debug(f"{args.operation} scraping '{url}'")
+    scraped = scrape(url)
     result = {}
-    result = scene_from_tree(scraped)
-    result["url"] = params["url"]
+    if args.operation == "performer":
+        result = performer_from_tree(scraped)
+        result["urls"].append(url)
+    elif args.operation == "scene":
+        result = scene_from_tree(scraped)
+        result["url"] = url
+
     print(json.dumps(result))
+
+if __name__ == "__main__":
+    main()
