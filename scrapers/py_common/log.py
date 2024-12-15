62714814b2d5f4353f38b6d696cf7c35170a4c14
Maista6969 <glad.beach5807@fastmail.com>
2024-12-15 19:14:39 +0100

    Add exception handler to py_common log

--- scrapers/py_common/log.py
+++ scrapers/py_common/log.py
@@ -1,5 +1,7 @@
 import sys
 import re
+import traceback
+from functools import partial
 # Log messages sent from a script scraper instance are transmitted via stderr and are
 # encoded with a prefix consisting of special character SOH, then the log
 # level (one of t, d, i, w or e - corresponding to trace, debug, info,
@@ -9,31 +11,32 @@ import re
 # The log.trace, log.debug, log.info, log.warning, and log.error methods, and their equivalent
 # formatted methods are intended for use by script scraper instances to transmit log
 # messages.
-#
-
-def __log(level_char: bytes, s):
-    if level_char:
-        lvl_char = "\x01{}\x02".format(level_char.decode())
-        s = re.sub(r"data:image.+?;base64(.+?')","[...]",str(s))
-        for x in s.split("\n"):
-            print(lvl_char, x, file=sys.stderr, flush=True)
 
 
-def trace(s):
-    __log(b't', s)
+def __log(level_char: str, s):
+    lvl_char = "\x01{}\x02".format(level_char)
+    s = re.sub(r"data:image.+?;base64(.+?')", "[...]", str(s))
+    print(lvl_char, s, file=sys.stderr, flush=True)
 
 
-def debug(s):
-    __log(b'd', s)
+trace = partial(__log, "t")
+debug = partial(__log, "d")
+info = partial(__log, "i")
+warning = partial(__log, "w")
+error = partial(__log, "e")
 
 
-def info(s):
-    __log(b'i', s)
+# Uncaught exceptions will result in unmarshaling errors which are not helpful
+# so we hook into the exception printing machinery: if users report a failure
+# with exit code 69 we can assume an uncaught exception instead of something like
+# an accidental print statement or malformed JSON output
+def custom_excepthook(exc_type, exc_value, exc_traceback):
+    for line in traceback.format_exception(exc_type, exc_value, exc_traceback):
+        error(line)
+    print("null")
+    exit(69)
 
 
-def warning(s):
-    __log(b'w', s)
-
-
-def error(s):
-    __log(b'e', s)
+# Assumption: only Stash will pipe into stdin
+if not sys.stdin.isatty():
+    sys.excepthook = custom_excepthook
