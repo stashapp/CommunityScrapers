db26238770cafae8d99dc924ee93d75a583dd7e0
Maista6969 <glad.beach5807@fastmail.com>
2025-01-13 17:56:59 +0100

    Add search to Loyalfans scraper

--- scrapers/Loyalfans/Loyalfans.py
+++ scrapers/Loyalfans/Loyalfans.py
@@ -1,179 +1,95 @@
-import os
 import sys
 import json
+import re
 
-# to import from a parent directory we need to add that directory to the system path
-csd = os.path.dirname(os.path.realpath(__file__))  # get current script directory
-parent = os.path.dirname(csd)  # parent directory (should be the scrapers one)
-sys.path.append(
-    parent
-)  # add parent dir to sys path so that we can import py_common from there
+from py_common import log
+from py_common.types import ScrapedScene
+from py_common.util import scraper_args, dig
+from py_common.deps import ensure_requirements
 
-try:
-    # Import Stash logging system from py_common
-    from py_common import log
-except ModuleNotFoundError:
-    print(
-        "You need to download the folder 'py_common' from the community repo. (CommunityScrapers/tree/master/scrapers/py_common)",
-        file=sys.stderr,
-    )
-    sys.exit()
+ensure_requirements("cloudscraper")
+import cloudscraper  # noqa: E402
 
-try:
-    # Import necessary modules.
-    import requests
-    import re
-
-# If one of these modules is not installed:
-except ModuleNotFoundError:
-    log.error("You need to install the python modules mentioned in requirements.txt")
-    log.error(
-        "If you have pip (normally installed with python), run this command in a terminal from the directory the scraper is located: pip install -r requirements.txt"
-    )
-    sys.exit()
-
-# Lookup table for tag replacements. The tags are in the form of hashtags, and often have multiple words mashed together.
-# This is a quick and dirty way of turning these into meaningful data, and can be expanded on to taste.
-TAG_REPLACEMENTS = {
-    "Fin Dom": "Findom",
-    "Fem Dom": "Femdom",
-    "bigtits": "Big Tits",
-    "titworship": "Tit Worship",
-    "financialdomination": "Financial Domination",
-    "R I P O F F": "ripoff",
-    "pussydenial": "pussy denial",
-}
-
-
-def output_json_url(title, tags, url, image, studio, performers, description, date):
-    # Create a tag dictionary from the tag list.
-    tag_dicts = [{"name": tag.strip(". ")} for tag in tags if tag.strip() != "N/A"]
-    # We're only using the value of 'performers' for our performer list
-    performer_dicts = [{"name": performer} for performer in performers]
-    # Dump all of this as JSON data.
-    return json.dumps(
-        {
-            "title": title,
-            "tags": tag_dicts,
-            "url": url,
-            "image": image,
-            "studio": {"name": studio},
-            "performers": performer_dicts,
-            "details": description,
-            "date": date,
-        },
-        indent=2,
-    )
-
-
-def get_cookies(scene_url: str):
-    # Establish a session.
-    session = requests.Session()
-    # Set headers required for a successful POST query.
-    headers = {
+scraper = cloudscraper.create_scraper()
+scraper.headers.update(
+    {
         "Accept": "application/json",
         "Accept-Language": "en-US,en;q=0.9",
         "Content-Type": "application/json",
         "Origin": "https://www.loyalfans.com",
-        "Referer": scene_url,
+        "Referer": "https://www.loyalfans.com",
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
     }
-    # URL of the system status API. This is called when a Loyalfans page is first loaded from what I can tell.
-    url = "https://www.loyalfans.com/api/v2/system-status"
-    # Perform a POST query to capture initial cookies.
-    response = session.post(url, headers=headers)
-    # Return these cookies.
-    return response.cookies
+)
+# Initial API request to get cookies
+scraper.post("https://www.loyalfans.com/api/v2/system-status")
 
 
-def get_api_url(scene_url: str):
-    # Extract the last component of the scene URL.
-    end_segment = scene_url.split("/")[-1]
-    # Append this to the API link. As far as I can tell, post names in scene URLs are unique. I have yet to encounter any data mismatches.
-    return f"https://www.loyalfans.com/api/v1/social/post/{end_segment}"
-
-
-def get_json(scene_url: str):
-    # Set headers required for a successful request.
-    headers = {
-        "Accept": "application/json",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Content-Type": "application/json",
-        "Origin": "https://www.loyalfans.com",
-        "Referer": scene_url,
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+def to_scraped_scene(scene_from_api: dict) -> ScrapedScene:
+    owner_slug = scene_from_api["owner"]["slug"]
+    scene_slug = scene_from_api["slug"]
+    scraped: ScrapedScene = {
+        "title": scene_from_api["title"],
+        "urls": [f"https://www.loyalfans.com/{owner_slug}/video/{scene_slug}"],
     }
-    # Set cookies using get_cookies function.
-    cookie_set = get_cookies(scene_url)
-    # Perform request using the API URL of the scene in question, adding headers and cookies.
-    response = requests.get(get_api_url(scene_url), headers=headers, cookies=cookie_set)
-    # Capture the response as JSON.
-    json_data = response.json()
-    # Return the JSON data.
-    return json_data
+
+    if content := dig(scene_from_api, "content"):
+        details = content.replace("<br />", "")
+        # Sometimes hashtags are included at the bottom of the description. This line strips all that junk out, as we're utilising the hashtags for the tags. Also tidies up ellipses.
+        details = re.sub(r"#\w+\b", "", details).replace(". . .", "...").strip()
+        scraped["details"] = details
+
+    if image := dig(scene_from_api, "video_object", "poster"):
+        scraped["image"] = image
+
+    if studio_name := dig(scene_from_api, "owner", "display_name"):
+        scraped["studio"] = {"name": studio_name}
+        scraped["performers"] = [{"name": studio_name}]
+
+    if date := dig(scene_from_api, "created_at", "date"):
+        scraped["date"] = date.split(" ")[0]
+
+    if tags := dig(scene_from_api, "hashtags"):
+        scraped["tags"] = [{"name": tag.strip("#. ")} for tag in tags]
+
+    # TODO: add duration to Stash scrapers
+    # if duration := dig(scene_from_api, "video_object", "duration"):
+    #     scraped["duration"] = duration
+
+    return scraped
 
 
-def scrape_scene(scene_url: str) -> dict:
-    # Capture JSON relating to this scene from the Loyalfans API.
-    json = get_json(scene_url)
-    # Extract title from the JSON and strip out any whitespace.
-    title = json["post"]["title"].strip()
-    # Use the video thumbnail/preview poster as the image.
-    image = json["post"]["video_object"].get("poster")
-    # Extract description, fix apostrophes and remove HTML newline tags.
-    description = json["post"]["content"].replace("\u2019", "'").replace("<br />", "")
-    # Sometimes hashtags are included at the bottom of the description. This line strips all that junk out, as we're utilising the hashtags for the tags. Also tidies up double-spacing and ellipses.
-    description = (
-        re.sub(r"#\w+\b", "", description)
-        .strip()
-        .replace("  ", " ")
-        .replace(". . .", "...")
-    )
-    # Extract studio name.
-    studio = json["post"]["owner"]["display_name"]
-    # Extract date. The JSON returns the date in the format '2023-06-18 12:00:00', but we only need the date, so the time is stripped out.
-    date = json["post"]["created_at"]["date"].split(" ")[0]
-    # Extract tags.
-    tags_list = json["post"]["hashtags"]
-    fixed_tags = []
-    # For every tag we find:
-    for tag in tags_list:
-        # Remove the hash from the start.
-        tag = tag[1:]
-        modified_tag = tag
-        # Split CamelCase tags into separate words.
-        modified_tag = re.sub(r"(?<!^)(?=[A-Z])", " ", tag).strip()
-        # Perform replacements according to the above lookup table.
-        for find, replace in TAG_REPLACEMENTS.items():
-            modified_tag = re.sub(
-                r"\b" + re.escape(find) + r"\b", replace, modified_tag
-            )
-        fixed_tags.append(modified_tag)
-
-    # LoyalFans doesn't provide a cast list so we'll just use the studio name as the performer name.
-    performers = [studio]
-
-    # Convert into meaningful JSON that Stash can use.
-    json_dump = output_json_url(
-        title, fixed_tags, scene_url, image, studio, performers, description, date
-    )
-
-    print(json_dump)
+def scene_from_url(scene_url: str) -> dict:
+    slug = scene_url.split("/")[-1]
+    api_url = f"https://www.loyalfans.com/api/v1/social/post/{slug}"
+    raw = scraper.get(api_url).json()
+    return to_scraped_scene(raw["post"])
 
 
-def main():
-    fragment = json.loads(sys.stdin.read())
-    url = fragment.get("url")
-    # If nothing is passed to the script:
-    if url is None:
-        log.error("No URL provided")
-        sys.exit(1)
-    # If we've been given a URL:
-    if url is not None:
-        scrape_scene(url)
+def scene_search(query: str) -> list[ScrapedScene]:
+    query_body = {
+        "q": query,
+        "type": "videos",
+        "limit": 10,
+    }
+    search_url = "https://www.loyalfans.com/api/v2/advanced-search?ngsw-bypass=true"
+    raw = scraper.post(search_url, json=query_body).json()
+    return [to_scraped_scene(scene) for scene in raw["data"]]
 
 
 if __name__ == "__main__":
-    main()
+    op, args = scraper_args()
 
-# Last updated 2023-06-18
+    result = None
+    match op, args:
+        case "scene-by-url" | "scene-by-query-fragment", {"url": url} if url:
+            result = scene_from_url(url)
+        case "scene-by-name", {"name": name} if name:
+            result = scene_search(name)
+        case _:
+            log.error(
+                f"Not implemented: Operation: {op}, arguments: {json.dumps(args)}"
+            )
+            sys.exit(1)
+
+    print(json.dumps(result))
