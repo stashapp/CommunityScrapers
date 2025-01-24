5b4199b4f14ef262ed31f6aad5023e8d74e7d30f
nrg101 <admin@amazinggoose.com>
2025-01-24 01:41:22 +0000

    add performerByName and performerByFragment

--- scrapers/Algolia/AlgoliaAPI.py
+++ scrapers/Algolia/AlgoliaAPI.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import json
 import re
 import sys
-from typing import Any, Callable
+from typing import Any, Callable, Dict, List
 from urllib.parse import urlparse
 
 import requests
@@ -15,7 +15,10 @@ from py_common.util import dig, guess_nationality, scraper_args
 
 from algoliasearch.search.client import SearchClientSync
 from algoliasearch.search.config import SearchConfig
+from algoliasearch.search.models.hit import Hit
+from algoliasearch.search.models.search_params import SearchParams
 
+IMAGE_CDN = "https://images03-fame.gammacdn.com"
 
 def get_api_auth(site: str) -> tuple[str, str]:
     homepage = f"https://www.{site}.com"
@@ -38,6 +41,10 @@ def get_api_auth(site: str) -> tuple[str, str]:
     return application_id, api_key
 
 
+def get_homepage(site: str) -> str:
+    return f"https://www.{site}.com"
+
+
 def get_search_client(site: str) -> SearchClientSync:
     # Get API auth and initialise client
     app_id, api_key = get_api_auth(site)
@@ -45,7 +52,7 @@ def get_search_client(site: str) -> SearchClientSync:
         app_id=app_id,
         api_key=api_key,
     )
-    homepage = f"https://www.{site}.com"
+    homepage = get_homepage(site)
     config.headers['Origin'] = homepage
     config.headers['Referer'] = homepage
     return SearchClientSync(config=config)
@@ -55,108 +62,51 @@ def default_postprocess(obj: Any, _) -> Any:
     return obj
 
 
-tags_map = {
-    90: "Athletic Woman",
-    107: "White Woman",
-    112: "Black Woman",
-    113: "European Woman",
-    121: "Latina Woman",
-    125: "Black Hair (Female)",
-    126: "Blond Hair (Female)",
-    127: "Brown Hair (Female)",
-    128: "Red Hair (Female)",
-    215: "Rimming Him",
-    274: "Rimming Her",
-    374: "Black Man",
-    376: "European Man",
-    377: "Latino Man",
-    378: "White Man",
-    379: "Black Hair (Male)",
-    380: "Blond Hair (Male)",
-    381: "Brown Hair (Male)",
-    383: "Red Hair (Male)",
-    385: "Shaved Head",
-    386: "Short Hair (Male)",
+genders_map = {
+    'shemale': 'transgender_female',
 }
-def to_tag(api_object: dict) -> ScrapedTag:
-    mapped_tag = tags_map.get(api_object["id"], api_object["name"].strip())
-    return {"name": mapped_tag}
 
 
-def to_tags(api_object: dict) -> list[ScrapedTag]:
-    tags = api_object.get("tags", [])
-    return [to_tag(x) for x in tags if "name" in x or x.get("id") in tags_map.keys()]
+def _construct_performer_url(p: Hit, site: str) -> str:
+    return f"https://www.{site}.com/en/pornstar/view/{p.url_name}/{p.actor_id}"
 
 
-def _construct_performer_url(api_result: dict, site: str) -> str:
-    id_ = api_result["id"]
-    slug = slugify(api_result["name"])
-    return f"https://www.{site}.com/model/{id_}/{slug}"
-
-
-## Helper functions to convert from Aylo's API to Stash's scaper return types
-def to_scraped_performer(
-    performer_from_api: dict, site: str | None = None
-) -> ScrapedPerformer:
-    if (type_ := dig(performer_from_api, "brand")) and type_ not in (
-        "actorsandtags",
-        # Older sites use this type
-        "phpactors",
-    ):
-        wrong_type = performer_from_api.get("type", "mystery")
-        wrong_id = performer_from_api.get("id", "unknown")
-        log.error(f"Attempted to scrape a '{wrong_type}' (ID: {wrong_id}) as a scene.")
-        raise ValueError("Invalid performer from API")
-    # This is all we get when scraped as part of a scene or movie
+## Helper functions to convert from Algolia's API to Stash's scraper return type
+def to_scraped_performer(performer_from_api: Hit, site: str) -> ScrapedPerformer:
     performer: ScrapedPerformer = {
-        "name": performer_from_api["name"],
-        "gender": performer_from_api["gender"],
+        "name": performer_from_api.name,
+        "gender": genders_map.get(performer_from_api.gender, performer_from_api.gender),
     }
 
-    if aliases := ", ".join(
-        alias
-        for alias in performer_from_api.get("aliases", [])
-        if alias.lower() != performer["name"].lower()
-    ):
-        performer["aliases"] = aliases
-
-    if details := performer_from_api.get("bio"):
+    if details := performer_from_api.description:
         performer["details"] = details
 
-    # All remaining fields are only available when scraped directly
-    if height := performer_from_api.get("height"):
-        # Convert to cm
-        performer["height"] = str(round(height * 2.54))
+    if eye_color := performer_from_api.attributes.get('eye_color'):
+        performer["eye_color"] = eye_color
 
-    if weight := performer_from_api.get("weight"):
-        # Convert to kg
-        performer["weight"] = str(round(weight / 2.205))
+    if hair_color := performer_from_api.attributes.get('hair_color'):
+        performer["hair_color"] = hair_color
 
-    if birthdate := performer_from_api.get("birthday"):
-        performer["birthdate"] = datetime.strptime(
-            birthdate, "%Y-%m-%dT%H:%M:%S%z"
-        ).strftime("%Y-%m-%d")
+    if ethnicity := performer_from_api.attributes.get('ethnicity'):
+        performer["ethnicity"] = ethnicity
 
-    if birthplace := performer_from_api.get("birthPlace"):
-        performer["country"] = guess_nationality(birthplace)
+    if alternate_names := performer_from_api.attributes.get('alternate_names'):
+        performer["aliases"] = alternate_names
 
-    if measurements := performer_from_api.get("measurements"):
-        performer["measurements"] = measurements
+    if height := performer_from_api.attributes.get('height'):
+        performer["height"] = height
 
-    images = dig(performer_from_api, "images", "master_profile") or {}
-    # Performers can have multiple images, try to get the biggest versions
-    if images := [
-        img
-        for alt in images.values()
-        if (img := dig(alt, ("xx", "xl", "lg", "md", "sm"), "url"))
-    ]:
-        performer["images"] = images
+    if weight := performer_from_api.attributes.get('weight'):
+        performer["weight"] = weight
+    
+    if home := performer_from_api.attributes.get('home'):
+        performer["country"] = home
 
-    if tags := to_tags(performer_from_api):
-        performer["tags"] = tags
+    if performer_from_api.has_pictures:
+        main_pic = list(performer_from_api.pictures.values())[-1]
+        performer["images"] = [f"{IMAGE_CDN}/actors{main_pic}"]
 
-    if site:
-        performer["urls"] = [_construct_performer_url(performer_from_api, site)]
+    performer["urls"] = [_construct_performer_url(performer_from_api, site)]
 
     return performer
 
@@ -182,20 +132,6 @@ def get_id(url: str) -> str:
     return match.group(1)
 
 
-def uri_index_query(index: str) -> str:
-    """
-    Produces the URI path for a single index query
-    """
-    return f"/1/indexes/{index}/query"
-
-
-def uri_one_record_by_id(index: str, id: str):
-    """
-    Produces the URI path for a single index object
-    """
-    return f"/1/indexes/{index}/{id}"
-
-
 def performer_from_url(
     url,
     postprocess: Callable[
@@ -217,29 +153,59 @@ def performer_from_url(
     response = client.search_single_index(
         index_name="all_actors_latest_desc",
         search_params={
-            "facetFilters": [f"actor_id:{actor_id}"]
-        }
+            "attributesToHighlight": [],
+            "filters": f"actor_id:{actor_id}",
+            "length": 1,
+        },
     )
 
-    log.debug(response)
+    log.debug(f"Number of search hits: {response.nb_hits}")
 
-    # # search performer by ID
-    # request_body = {
-    #     "params": "hitsPerPage=20&page=0&query=",
-    #     "facetFilters": [f"actor_id:{actor_id}"]
-    # }
-    # res = session.post(uri_index_query("all_actors_latest_desc"), json=request_body)
-    # log.debug(res.status_code)
-    # log.debug(res.text)
-
+    if response.nb_hits:
+        return to_scraped_performer(response.hits[0], site)
     return {}
     # return postprocess(to_scraped_performer(api_performer_json), api_performer_json)
+
+
+def performer_search(name: str, sites: List[str]) -> list[ScrapedPerformer]:
+    site = sites[0]
+    # Get API auth and initialise client
+    client = get_search_client(site)
+
+    response = client.search_single_index(
+        index_name="all_actors_latest_desc",
+        search_params={
+            "attributesToHighlight": [],
+            "query": name,
+            "length": 20,
+        },
+    )
+
+    log.debug(f"Number of search hits: {response.nb_hits}")
+
+    if response.nb_hits:
+        return [ to_scraped_performer(hit, site) for hit in response.hits ]
+    return []
+
+
+def performer_from_fragment(args: Dict[str, Any]) -> ScrapedPerformer:
+    """
+    This receives:
+    - name
+    - urls
+    - gender
+    from the result of the performer-by-name search
+    """
+    # the first URL should be usable for a full search
+    url = args.get("urls")[0]
+    return performer_from_url(url)
 
 
 if __name__ == "__main__":
     op, args = scraper_args()
     result = None
 
+    log.debug(f"args: {args}")
     match op, args:
         # case "gallery-by-url", {"url": url} if url:
         #     url = redirect(url)
@@ -260,19 +226,19 @@ if __name__ == "__main__":
         #         args, search_domains=domains, postprocess=bangbros
         #     )
         case "performer-by-url", {"url": url}:
-            # url = redirect(url)
             result = performer_from_url(url)
-        # case "performer-by-fragment", args:
-        #     result = performer_from_fragment(args)
-        # case "performer-by-name", {"name": name} if name:
-        #     result = performer_search(
-        #         name, search_domains=domains, postprocess=bangbros
-        #     )
+        case "performer-by-fragment", args:
+            result = performer_from_fragment(args)
+        case "performer-by-name", {"name": name, "extra": extra} if name and extra:
+            sites = extra
+            result = performer_search(name, sites)
         # case "movie-by-url", {"url": url} if url:
         #     url = redirect(url)
         #     result = movie_from_url(url, postprocess=bangbros)
         case _:
             log.error(f"Operation: {op}, arguments: {json.dumps(args)}")
             sys.exit(1)
+
+    log.debug(f"result: {result}")
 
     print(json.dumps(result))
