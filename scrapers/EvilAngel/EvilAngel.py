e01274947ef8778a7dda017d426018e8114b359c
nrg101 <admin@amazinggoose.com>
2025-01-27 15:18:35 +0000

    make studio determination work for all sub-studios sceneByURL

--- scrapers/EvilAngel/EvilAngel.py
+++ scrapers/EvilAngel/EvilAngel.py
@@ -18,41 +18,96 @@ from py_common import log
 from py_common.types import ScrapedScene
 from py_common.util import scraper_args
 
+channel_name_map = {
+    "AnalPlaytime": "Anal Acrobats",
+    "Anal Trixxx": "AnalTriXXX",
+    "Jonni Darkko ": "Jonni Darkko XXX",    # trailing space is in the API
+    "Le Wood": "LeWood",
+    "Secret Crush ": "Secret Crush",    # trailing space is in the API
+}
+"""
+This map just contains overrides when using a channel name as the studio
+"""
 
-def postprocess_evilangel_scene(scene: ScrapedScene, api_scene: dict[str, Any]) -> ScrapedScene:
-    available_on_site = api_scene.get("availableOnSite", [])
-    main_channel_name = api_scene.get("mainChannel", {}).get("name")
-    serie_name = api_scene.get("serie_name")
-    sitename_pretty = api_scene.get("sitename_pretty")
+serie_name_map = {
+    "TransPlaytime": "TS Playground",
+    "XXXmailed": "Blackmailed",
+}
+"""
+Each serie_name found in the logic should have a key-value here
+"""
+
+site_map = {
+    "christophclarkonline": "Christoph Clark Online",
+    "christophsbignaturaltits": "Christoph's Big Natural Tits",
+    "gapingangels": "Gaping Angels",
+    "iloveblackshemales": "I Love Black Shemales",
+    "jakemalone": "Jake Malone",
+    "johnleslie": "John Leslie",
+    "lexingtonsteele": "Lexington Steele",
+    "nachovidalhardcore": "Nacho Vidal Hardcore",
+    "pansexualx": "PansexualX",
+    "pantypops": "Panty Pops",
+    "povblowjobs": "POV Blowjobs",
+    "roccosiffredi": "Rocco Siffredi",
+    "sheplayswithhercock": "She Plays With Her Cock",
+    "strapattackers": "Strap Attackers",
+    "tittycreampies": "Titty Creampies",
+    "transgressivexxx": "TransgressiveXXX",
+    "tsfactor": "TS Factor",
+}
+"""
+Each site found in the logic should have a key-value here
+"""
+
+def determine_studio(api_object: dict[str, Any]) -> str | None:
+    available_on_site = api_object.get("availableOnSite", [])
+    main_channel_name = api_object.get("mainChannel", {}).get("name")
+    serie_name = api_object.get("serie_name")
     log.debug(
         f"available_on_site: {available_on_site}, "
         f"main_channel_name: {main_channel_name}, "
         f"serie_name: {serie_name}, "
-        f"sitename_pretty: {sitename_pretty}"
     )
 
     # determine studio override with custom logic
-    if main_channel_name in [
-        "Le Wood",
-        "TransgressiveXXX",
-    ]:
-        scene["studio"] = { "name": main_channel_name }
-    elif serie_name in [
-        "TransPlaytime"
-    ]:
-        scene["studio"] = { "name": serie_name }
-    elif sitename_pretty in [
-        "PansexualX",
-    ]:
-        scene["studio"] = { "name": sitename_pretty }
-    elif site_match := next(
-        (item for item in [
-            "tsfactor"
-        ] if item in available_on_site),
+    # steps through from api_scene["availableOnSite"], and picks the first match
+    if site_match := next(
+        (item for item in site_map.keys() if item in available_on_site),
         None
     ):
-        if channel_name := next((channel["name"] for channel in api_scene["channels"] if channel["id"] == site_match)):
-            scene["studio"] = { "name": channel_name }
+        return site_map.get(site_match, site_match)
+    elif serie_name in serie_name_map.keys():
+        return serie_name_map.get(serie_name, serie_name)
+    elif main_channel_name in [
+        "AnalPlaytime",
+        "Anal Trixxx",
+        "Buttman",
+        "Cock Choking Sluts",
+        "Euro Angels",
+        "Jonni Darkko ",    # trailing space is in the API
+        "Le Wood",
+        "Secret Crush ",    # trailing space is in the API
+        "Transsexual Angel",
+    ]:
+        return channel_name_map.get(main_channel_name, main_channel_name)
+    elif director_match := next(
+        (item for item in [
+            "Joey Silvera",
+            "Mike Adriano",
+        ] if item in [c.get("name") for c in api_object.get("channels")]),
+        None
+    ):
+        return director_match
+    elif movie_desc := api_object.get("movie_desc"):
+        if "BAM Visions" in movie_desc:
+            return "BAM Visions"
+    return None
+
+
+def postprocess_evilangel_scene(scene: ScrapedScene, api_scene: dict[str, Any]) -> ScrapedScene:
+    if studio_override := determine_studio(api_scene):
+        scene["studio"] = { "name": studio_override }
 
     return scene
 
