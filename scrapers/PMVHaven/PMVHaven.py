stashapp/CommunityScrapers

commit ec7813fd0539e2b3c33fd58e1808c736592c0898

bugfixes (#2156)

scrapers/PMVHaven/PMVHaven.py
@@ -73,14 +73,14 @@ def getVideoById(sceneId):
     }
 
     if description := dig(video, "description"):
-        scraped["description"] = description
+        scraped["details"] = description
 
     if songs := dig(video, "music"):
         music = "Music:\n" + "\n".join(songs)
         if "description" in scraped:
-            scraped["description"] += "\n" + music
+            scraped["details"] += "\n" + music
         else:
-            scraped["description"] = music
+            scraped["details"] = music
 
     if creator := dig(video, "creator"):
         scraped["studio"] = {"name": creator}
@@ -90,6 +90,7 @@ def getVideoById(sceneId):
     scraped["tags"] = sorted(
         {tag.strip().lower(): tag.strip() for tag in tags}.values()
     )
+    scraped["tags"] = [{"name": tagName} for tagName in scraped["tags"]]
 
     return scraped
 

commit cbd8b9a7ccaa16bc932542427148ef3f8ff8a3f7

[PMVHaven] fix details regression
closes #2164
regression from #2156

scrapers/PMVHaven/PMVHaven.py
@@ -77,7 +77,7 @@ def getVideoById(sceneId):
 
     if songs := dig(video, "music"):
         music = "Music:\n" + "\n".join(songs)
-        if "description" in scraped:
+        if "details" in scraped:
             scraped["details"] += "\n" + music
         else:
             scraped["details"] = music