4e33f3656e03f27d09b99a378919f721741b2147
goawaylovestrike <goawaylovestrike@gmail.com>
2025-03-02 22:32:24 -0800

    Update Coomer.py

--- scrapers/Coomer/Coomer.py
+++ scrapers/Coomer/Coomer.py
@@ -20,7 +20,7 @@ def extract_mentions_and_tags(text):
     return mentions, hashtags
 
 def debugPrint(t):
-    sys.stderr.write(t + "\n")
+    sys.stderr.write(t + '\n')
 
 # Get JSON from Stash
 def readJSONInput():
@@ -28,13 +28,22 @@ def readJSONInput():
     input = sys.stdin.read()
     return json.loads(input)
 
+def custom_title(text):
+    def capitalize(match):
+        word = match.group()
+        if "'" in word:
+            parts = word.split("'")
+            return parts[0].capitalize() + "'" + parts[1]
+        return word.capitalize()
+    return re.sub(r"\b\w+('\w+)?", capitalize, text)
+
 def clean_text(details: str) -> (str, str):
     """
     remove escaped backslashes and html parse the details text
     """
     if details:
-        details = re.sub(r"\\", "", details)
-        details = re.sub(r"<\s*/?br\s*/?\s*>", "\n",
+        details = re.sub(r'\\', '', details)
+        details = re.sub(r'<\s*/?br\s*/?\s*>', '\n',
                          details)  # bs.get_text doesnt replace br's with \n
         details = re.sub(r'</?p>', '\n', details)
         details = bs(details, features='html.parser').get_text()
@@ -46,21 +55,21 @@ def clean_text(details: str) -> (str, str):
         )
         details = details.strip()
         lines = details.split('\n')
-        first_line = lines[0] if lines else ""
+        first_line = lines[0] if lines else ''
         
         if len(first_line) > 100:
             # Consider only the first 100 characters for truncation
             first_100_chars = first_line[:100]
             # Regular expression to match common emoji patterns
             emoji_pattern = re.compile(
-                "["
-                "\U0001F600-\U0001F64F"  # emoticons
-                "\U0001F300-\U0001F5FF"  # symbols & pictographs
-                "\U0001F680-\U0001F6FF"  # transport & map symbols
-                "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                "\U00002702-\U000027B0"  # Dingbats
-                "\U000024C2-\U0001F251" 
-                "]+", flags=re.UNICODE
+                '['
+                '\U0001F600-\U0001F64F'  # emoticons
+                '\U0001F300-\U0001F5FF'  # symbols & pictographs
+                '\U0001F680-\U0001F6FF'  # transport & map symbols
+                '\U0001F1E0-\U0001F1FF'  # flags (iOS)
+                '\U00002702-\U000027B0'  # Dingbats
+                '\U000024C2-\U0001F251' 
+                ']+', flags=re.UNICODE
             )
             match = emoji_pattern.search(first_100_chars)
             if match:
@@ -76,20 +85,20 @@ def clean_text(details: str) -> (str, str):
                     else:
                         truncated_first_line = first_100_chars
 
-            rest_of_details = '\n'.join(lines[1:]).strip() if len(lines) > 1 else ""
+            rest_of_details = '\n'.join(lines[1:]).strip() if len(lines) > 1 else ''
             rest_of_details = truncated_first_line + '\n' + rest_of_details
-            first_line = first_100_chars
+            first_line = truncated_first_line
         else:
-            rest_of_details = '\n'.join(lines[1:]).strip() if len(lines) > 1 else ""
+            rest_of_details = '\n'.join(lines[1:]).strip() if len(lines) > 1 else ''
 
-        first_line = first_line.title()  
+        first_line = custom_title(first_line)
 
         return first_line, rest_of_details
-    return "", ""
+    return '', ''
 
 def user_query (service, user):
     if re.match('[0-9]*', user): 
-        coomer_getuser_url = f"https://coomer.su/api/v1/{service}/user/{user}/profile"
+        coomer_getuser_url = f'https://coomer.su/api/v1/{service}/user/{user}/profile'
         log.debug(coomer_getuser_url)
         user_lookup_response = requests.get(coomer_getuser_url, headers=headers)
         if user_lookup_response.status_code == 200:
@@ -99,7 +108,7 @@ def user_query (service, user):
     return user
        
 def post_query(service, user_id, id):
-    coomer_getpost_url = f"https://coomer.su/api/v1/{service}/user/{user_id}/post/{id}"
+    coomer_getpost_url = f'https://coomer.su/api/v1/{service}/user/{user_id}/post/{id}'
     post_lookup_response = requests.get(coomer_getpost_url, headers=headers)
 
     if post_lookup_response.status_code == 200:
@@ -108,15 +117,15 @@ def post_query(service, user_id, id):
         post = data['post']
         user_name = user_query(service, user_id)
         
-        if service == "onlyfans":
-            studio = {"Name": f"{user_name} (OnlyFans)", "URL": f"https://onlyfans.com/{user_name}"}
-        elif service == "fansly":
-            studio = {"Name": f"{user_name} (Fansly)", "URL": f"https://fansly.com/{user_name}"}
-        elif service == "candfans":
-            studio = {"Name": f"{user_name} (CandFans)", "URL": f"https://candfans.com/{user_name}"}
+        if service == 'onlyfans':
+            studio = {'Name': f'{user_name} (OnlyFans)', 'URL': f'https://onlyfans.com/{user_name}'}
+        elif service == 'fansly':
+            studio = {'Name': f'{user_name} (Fansly)', 'URL': f'https://fansly.com/{user_name}'}
+        elif service == 'candfans':
+            studio = {'Name': f'{user_name} (CandFans)', 'URL': f'https://candfans.com/{user_name}'}
         else:
-            studio = {"Name": user_name}
-            debugPrint("No service listed")
+            studio = {'Name': f'{user_name}'}
+            debugPrint('No service listed')
 
         mentions, hashtags = extract_mentions_and_tags(post.get('content', ''))
 
@@ -124,20 +133,20 @@ def post_query(service, user_id, id):
         unique_performers.update(mentions)  
 
         if post['tags'] is not None:
-            tags = [{"name": item} for item in post['tags']]
+            tags = [{'name': item} for item in post['tags']]
         else:
-            tags = [{"name": tag} for tag in hashtags]
+            tags = [{'name': tag} for tag in hashtags]
 
         first_line, rest_of_details = clean_text(post['content'])
 
         out = {
-            "Title": first_line,
-            "Date": post['published'].split('T')[0],
-            "URL": f"https://coomer.su/{post['service']}/user/{post['user']}/post/{post['id']}",
-            "Details": rest_of_details,
-            "Studio": studio,
-            "Performers": [{"Name": name, "urls": [studio['URL']]} for name in unique_performers],
-            "Tags": tags,
+            'Title': first_line,
+            'Date': post['published'].split('T')[0],
+            'URL': f'https://coomer.su/{post["service"]}/user/{post["user"]}/post/{post["id"]}',
+            'Details': rest_of_details,
+            'Studio': studio,
+            'Performers': [{'Name': name, 'urls': [studio['URL']]} for name in unique_performers],
+            'Tags': tags,
         }
 
         log.debug(out)
@@ -160,12 +169,12 @@ def get_scene(inputurl):
 
 def sceneByFragment(fragment):
     file = fragment[0]
-    with open(file["path"], "rb") as f:
+    with open(file['path'], 'rb') as f:
         bytes = f.read()
         readable_hash = hashlib.sha256(bytes).hexdigest()
-        log.debug(f"sha256 hash: {readable_hash}")
+        log.debug(f'sha256 hash: {readable_hash}')
 
-    coomer_searchhash_url = "https://coomer.su/api/v1/search_hash/"
+    coomer_searchhash_url = 'https://coomer.su/api/v1/search_hash/'
 
     hash_lookup_response = requests.get(coomer_searchhash_url + str(readable_hash), headers=headers)
 
@@ -177,19 +186,19 @@ def sceneByFragment(fragment):
         return post_query(post['service'], post['user'], post['id'])
 
     else:
-        debugPrint("The hash of the file was not found. Please make sure you are using an original file.")
+        debugPrint('The hash of the file was not found. Please make sure you are using an original file.')
 
 
 if sys.argv[1] == 'sceneByURL':
     i = readJSONInput()
     log.debug(i)
     ret = get_scene(i.get('url'))
-    log.debug(f"Returned from search: {json.dumps(ret)}")
+    log.debug(f'Returned from search: {json.dumps(ret)}')
     print(json.dumps(ret))
 
 if sys.argv[1] == 'sceneByFragment':
     i = readJSONInput()
-    log.debug(f"Existing scene data: {json.dumps(i)}")
-    ret = sceneByFragment(i["files"])
-    log.debug(f"Returned from search: {json.dumps(ret)}")
+    log.debug(f'Existing scene data: {json.dumps(i)}')
+    ret = sceneByFragment(i['files'])
+    log.debug(f'Returned from search: {json.dumps(ret)}')
     print(json.dumps(ret))
