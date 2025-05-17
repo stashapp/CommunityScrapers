5952e2b7d785d718dcd5ed063911da57da0e46a0
Maista6969 <glad.beach5807@fastmail.com>
2025-05-18 00:35:13 +0200

    Update PetersKingdom
    Uses the argument parser from py_common for easier commandline usage

--- scrapers/PetersKingdom/PetersKingdom.py
+++ scrapers/PetersKingdom/PetersKingdom.py
@@ -1,16 +1,23 @@
-import py_common.log as log
 import json
 import re
-import requests
 import sys
-from lxml import html
+
+import py_common.log as log
+from py_common.deps import ensure_requirements
+from py_common.util import scraper_args, dig
+
+ensure_requirements("requests", "lxml")
+from lxml import html  # noqa: E402
+import requests  # noqa: E402
 
 session = requests.Session()
+
 
 def url_json(url: str):
     req = session.get(url)
     data = req.json()
     return data
+
 
 def scrape_scene_tags(hostname: str, postid: int):
     tags_url = f"https://{hostname}/wp-json/wp/v2/video-categories?post={postid}"
@@ -19,21 +26,18 @@ def scrape_scene_tags(hostname: str, postid: int):
         tags_url = f"https://{hostname}/wp-json/wp/v2/video-category?post={postid}"
     tags_data = url_json(tags_url)
     log.debug(f"Tags URL: {tags_url}")
-    tags = []
-    for tag in tags_data:
-        tags.append({ "Name": tag["name"] })
-    return tags
+    return [{"name": p["name"] for p in tags_data}]
+
 
 def scrape_scene_performers(url):
     performers_data = url_json(url)
-    performers = []
-    for performer in performers_data:
-        performers.append({ "Name": performer["name"] })
-    return performers
+    return [{"name": p["name"] for p in performers_data}]
+
 
 def scrape_scene_media(url):
     media_data = url_json(url)
     return media_data["guid"]["rendered"]
+
 
 def scrape_scene_video(url: str):
     """Scrape the /wp-json/videos endpoint for the scene
@@ -43,17 +47,25 @@ def scrape_scene_video(url: str):
     hostname = url.split("/")[2]
     # start constructing with sub-scrapers
     studio_data = url_json(f"https://{hostname}/wp-json")
-    cleaned_details = scene_data["content"]["rendered"].replace("<p>", "").replace("</p>", "").replace("</span>", "").strip()
+    cleaned_details = (
+        scene_data["content"]["rendered"]
+        .replace("<p>", "")
+        .replace("</p>", "")
+        .replace("</span>", "")
+        .strip()
+    )
     scene = {
-        "Date": scene_data["date"].split("T")[0],
-        "Title": scene_data["title"]["rendered"],
-        "Details": re.sub(r"<span .+>", "", cleaned_details),
-        "Studio": {
+        "title": scene_data["title"]["rendered"],
+        "date": scene_data["date"].split("T")[0],
+        "details": re.sub(r"<span .+>", "", cleaned_details),
+        "tags": scrape_scene_tags(hostname, scene_data["id"]),
+        "image": scrape_scene_media(
+            scene_data["_links"]["wp:featuredmedia"][0]["href"]
+        ),
+        "studio": {
             # urldecode for peter's kingdom
-            "Name": studio_data["name"].replace("&#039;", "'")
+            "name": studio_data["name"].replace("&#039;", "'")
         },
-        "Tags": scrape_scene_tags(hostname, scene_data["id"]),
-        "Image": scrape_scene_media(scene_data["_links"]["wp:featuredmedia"][0]["href"]),
     }
     # pull performers link if exists
     wp_terms = scene_data["_links"]["wp:term"]
@@ -65,21 +77,17 @@ def scrape_scene_video(url: str):
         performers_url = None
     if performers_url:
         performers_data = url_json(performers_url)
-        performers = []
-        for performer in performers_data:
-            performers.append({ "Name": performer["name"] })
-        scene["Performers"] = performers
+        scene["performers"] = [{"name": p["name"]} for p in performers_data]
     # return to handler
     return scene
 
+
 def scrape_scene_performers_lxml(body):
     # fallback for performers
+    performers = "//span[contains(.,'Starring')]/a | //a[contains(@href,'performers') and @class='jet-listing-dynamic-terms__link']"
     tree = html.fromstring(body)
-    performers = []
-    for performer in tree.xpath("//span[contains(.,'Starring')]/a | //a[contains(@href,'performers') and @class='jet-listing-dynamic-terms__link']"):
-        performer_name = performer.text_content().strip()
-        performers.append({ "Name": performer_name })
-    return performers
+    return [{"name": p.text_content().strip()} for p in tree.xpath(performers)]
+
 
 def scrape_wp_scene(url: str):
     """Scrape the wordpress page for /wp-json endpoints
@@ -90,26 +98,30 @@ def scrape_wp_scene(url: str):
     if site_data.status_code != 200:
         log.error(f"Error fetching {url}: {site_data.status_code}")
         return None
-    wp_json_url = re.search(r'type="application\/json" href="(.+?\/wp-json\/wp\/v2\/videos\/\d+)"', site_data.text)
-    if wp_json_url:
-        # found the URL, descend into the scraper pits
-        scraped = scrape_scene_video(wp_json_url[1])
-        if scraped:
-            # if performers are empty, scrape with lxml
-            if not scraped.get("Performers"):
-                log.debug("No performers found, scraping with lxml")
-                scraped["Performers"] = scrape_scene_performers_lxml(site_data.text)
-            print(json.dumps(scraped))
-    else:
-        log.error(f"Error: No wp-json URL found in {url}, site is probably incompatible")
+
+    pattern = r'type="application\/json" href="(.+?\/wp-json\/wp\/v2\/videos\/\d+)"'
+    if not (wp_json_url := re.search(pattern, site_data.text)):
+        log.error(f"No wp-json URL found in {url}, site is probably incompatible")
         return None
 
-def main():
-    if sys.argv[1] == "scene":
-        FRAGMENT = json.loads(sys.stdin.read())
-        url = FRAGMENT.get("url")
-        # scrape url
-        scrape_wp_scene(url)
+    # found the URL, descend into the scraper pits
+    scraped = scrape_scene_video(wp_json_url[1])
+    # if performers are empty, scrape with lxml
+    if not dig(scraped, "performers"):
+        log.debug("No performers found, scraping with lxml")
+        scraped["performers"] = scrape_scene_performers_lxml(site_data.text)
+
+    return scraped
+
 
 if __name__ == "__main__":
-    main()
+    op, args = scraper_args()
+    result = None
+    match op, args:
+        case "scene-by-url", {"url": url}:
+            result = scrape_wp_scene(url)
+        case _:
+            log.error(f"Operation: {op}, arguments: {json.dumps(args)}")
+            sys.exit(1)
+
+    print(json.dumps(result))
