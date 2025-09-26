2cba84de1d6e6dadff552a3d36031fb874c83420
feederbox826 <feederbox826@users.noreply.github.com>
2025-09-25 21:29:41 -0400

    [MilfVR] clean up py script
    - use requests, not cloudscraper since no CF
    - drop all unncessary headers
    - add image, typing to search result
    - hardcode name into function since it uses MIlfVR-specific methods

--- scrapers/MilfVr/MilfVr.py
+++ scrapers/MilfVr/MilfVr.py
@@ -1,51 +1,45 @@
 import json
 import sys
-import urllib
-import cloudscraper
+import urllib.parse
+import requests
 
 from py_common import log
 from py_common.util import scraper_args
+from py_common.types import SceneSearchResult
 
-scraper = cloudscraper.create_scraper()
+scraper = requests.Session()
 
-def searchSceneByName(name: str) -> list:
-    myUrl = "https://www.milfvr.com/search/suggest.json?group=yes&q=" + urllib.parse.quote(name)
-    log.debug(f"myUrl is: {myUrl}")
+def milfVRsearchSceneByName(name: str) -> list:
+    prep_cookies = scraper.get(url='https://milfvr.com')
+    site_cookies = prep_cookies.cookies.get_dict()
+    scraper.headers.update({
+       'x-csrf-token': site_cookies['csrfst']
+    })
+    scraper.cookies.update(site_cookies)
 
-    getCookies = scraper.get(url='https://milfvr.com')
+    search_url = "https://www.milfvr.com/search/suggest.json?group=yes&q=" + urllib.parse.quote(name)
+    search_result = scraper.get(url=search_url).json()
+    log.debug(f"Payload is: {search_result}")
 
-    myCookies = getCookies.cookies.get_dict()
-    log.debug(f"My cookies are: {myCookies}")
-    
-    myHeaders = {
-          'priority': 'u=1, i'
-          , 'referer': 'https://www.milfvr.com'
-          , 'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "Brave";v="140"'
-          , 'sec-ch-ua-arch': '"x86"'
-          , 'sec-ch-ua-full-version-list': '"Chromium";v="140.0.0.0", "Not=A?Brand";v="24.0.0.0", "Brave";v="140.0.0.0"'
-          , 'sec-ch-ua-mobile': '?0'
-          , 'sec-ch-ua-model': '""'
-          , 'sec-ch-ua-platform': '"Linux"'
-          , 'sec-ch-ua-platform-version': '"6.12.43"'
-          , 'sec-fetch-dest': 'empty'
-          , 'sec-fetch-mode': 'cors'
-          , 'sec-fetch-site': 'same-origin'
-          , 'sec-gpc': '1'
-          , 'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36'
-          , 'x-csrf-token': myCookies['csrfst']
-          , 'x-requested-with': 'XMLHttpRequest'
-        }
-    log.debug(f"My headers are: {myHeaders}")
+    # player-config experiment
+    # files = {
+    #     'item_id': (None, '6351323'),
+    # }
+    # debug with player config
+    # player = scraper.post(url=f"https://www.{HOSTNAME}/ajax/player-config.json", files=files)
+    # player_data = player.json()
+    # print(f"Player config: {player.json()}")
 
-    result = scraper.get(url=myUrl, headers=myHeaders, cookies=myCookies).json()
-    log.debug(f"Payload is: {result}")
-
-    returnData = []
-    for hit in result:
+    returnData: list[SceneSearchResult] = []
+    for hit in search_result:
         if ('icon' in hit and hit['icon'] == 'movie'):
-          s = {}
-          s['title'] = hit['label']
-          s['url'] = 'https://milfvr.com' + hit['url']
+          scene_id = hit['url'].split('-')[-1]
+          thumb_url = f"https://images.povr.com/general/{scene_id[0]}/{scene_id[0:4]}/{scene_id}/cover/large.webp"
+          s: SceneSearchResult = {
+              'title': hit['label'],
+              'url': f"https://milfvr.com{hit['url']}",
+              'image': thumb_url
+          }
           returnData.append(s)
 
     return returnData
@@ -56,7 +50,7 @@ if __name__ == "__main__":
 
     match op, args:
         case "scene-by-name", {"name": name} if name:
-            returnValue = searchSceneByName(name)
+            returnValue = milfVRsearchSceneByName(name)
         case _:
             log.error(f"Operation: {op}, arguments: {json.dumps(args)}")
             sys.exit(1)
