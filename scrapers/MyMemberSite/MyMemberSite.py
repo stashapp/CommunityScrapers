9bb59366dbed934aedbe04c30c481cb4ecc79714
Rubber Roman <rubber.roman98@gmail.com>
2025-10-04 23:26:42 +0200

    MyMemberSite: support all sites, fetch studio info from API, parse API URL from HTML response
    Signed-off-by: Rubber Roman <rubber.roman98@gmail.com>

--- scrapers/MyMemberSite/MyMemberSite.py
+++ scrapers/MyMemberSite/MyMemberSite.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse
 from py_common import log
 from py_common.deps import ensure_requirements
 from py_common.util import scraper_args, dig
-from py_common.types import ScrapedScene, ScrapedGallery
+from py_common.types import ScrapedGallery, ScrapedPerformer, ScrapedScene, ScrapedStudio, ScrapedTag
 
 ensure_requirements("requests", "bs4:beautifulsoup4")
 
@@ -16,429 +16,34 @@ from bs4 import BeautifulSoup  # noqa: E402
 session = requests.Session()
 session.headers.update(
     {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:143.0) Gecko/20100101 Firefox/143.0"
     }
 )
 
-# Maps public URLs to a API base URLs as well as pretty studio names
-_studios = {
-    "aeriefans.com": {
-        "api": "aerie-saunders.mymember.site",
-        "name": "Aerie Saunders",
-    },
-    "aglaeaproductions.com": {
-        "api": "aglaeaproductions.mymember.site",
-        "name": "Aglaea X",
-    },
-    "aliceshawbelly.com": {
-        "api": "aliceshawbelly.mymember.site",
-        "name": "Alice Shaw",
-    },
-    "allinthestepfamily.com": {
-        "api": "all-in-the-step-family.mymember.site",
-        "name": "All in the Step-Family",
-    },
-    "alterpic.com": {
-        "api": "alterpic.mymember.site",
-        "name": "Alterpic",
-    },
-    "amieesfetishhouse.com": {
-        "api": "amiees-fetish-house.mymember.site",
-        "name": "Amiees Fetish House",
-    },
-    "anistarxxx.com": {
-        "api": "anistarxxx.mymember.site",
-        "name": "AniStarXXX",
-    },
-    "asianmassagemaster.com": {
-        "api": "asianmassagemaster.mymember.site",
-        "name": "Asian Massage Master",
-    },
-    "bbwxxxadventures.com": {
-        "api": "bbwxxxadventures.mymember.site",
-        "name": "BBW XXX Adventures",
-    },
-    "bdsmkinkyplay.com": {
-        "api": "bdsmkinkyplay.mymember.site",
-        "name": "BDSM Kinky Play",
-    },
-    "beverlybluexxx.com": {
-        "api": "beverlybluexxx.mymember.site",
-        "name": "BeverlyBlueXxX",
-    },
-    "bindastimesuk.com": {
-        "api": "bindastimesuk.mymember.site",
-        "name": "Bindastimesuk",
-    },
-    "bondageliberation.com": {
-        "api": "bondageliberation.mymember.site",
-        "name": "Bondage Liberation",
-    },
-    "brookesballoons.com": {
-        "api": "brookesballoons.mymember.site",
-        "name": "BrookesBalloons",
-    },
-    "castersworldwide.com": {
-        "api": "castersworldwide.mymember.site",
-        "name": "Casters Worldwide",
-    },
-    "chloestoybox.com": {
-        "api": "chloestoybox.mymember.site",
-        "name": "Chloe Toy",
-    },
-    "clubsteffi.fun": {
-        "api": "clubsteffi.mymember.site",
-        "name": "ClubSteffi",
-    },
-    "cristalkinky.com": {
-        "api": "cristalkinky.mymember.site",
-        "name": "Cristal Kinky",
-    },
-    "cruel-women.com": {
-        "api": "cruel-women.mymember.site",
-        "name": "Cruel Women",
-    },
-    "curvymary.com": {
-        "api": "curvy-mary.mymember.site",
-        "name": "Curvy Mary",
-    },
-    "dannijones.com": {
-        "api": "dannijones.mymember.site",
-        "name": "Danni Jones",
-    },
-    "deemariexxx.com": {
-        "api": "deemariexxx.mymember.site",
-        "name": "Dee Marie",
-    },
-    "dirtydemi.com": {
-        "api": "dirtydemi.mymember.site",
-        "name": "Dirty Demi",
-    },
-    "europornvids.com": {
-        "api": "europornvids.mymember.site",
-        "name": "Euro Porn Vids",
-    },
-    "faexcheta.com": {
-        "api": "faexcheta.mymember.site",
-        "name": "Fae and Cheta",
-    },
-    "fetiliciousfans.com": {
-        "api": "fetilicousfans.mymember.site",
-        "name": "Miss Fetilicious",
-    },
-    "fetishchimera.com": {
-        "api": "fetishchimera.mymember.site",
-        "name": "Fetish Chimera",
-    },
-    "friskyfairyk.com": {
-        "api": "friskyfairyk.mymember.site",
-        "name": "xoXokmarie",
-    },
-    "girlsofhel.com": {
-        "api": "girls-of-hel.mymember.site",
-        "name": "Girls of HEL",
-    },
-    "glass-dp.com": {
-        "api": "glassdp.mymember.site",
-        "name": "Glassdp",
-    },
-    "glassdeskproductions.com": {
-        "api": "glassdeskproductions.mymember.site",
-        "name": "GlassDeskProductions",
-    },
-    "goddesslesley.com": {
-        "api": "goddesslesley.mymember.site",
-        "name": "Goddess Lesley",
-    },
-    "goddessrobin.com": {
-        "api": "goddessrobin.mymember.site",
-        "name": "Goddess Robin",
-    },
-    "greatbritishfeet.com": {
-        "api": "greatbritishfeet.mymember.site",
-        "name": "Great British Feet",
-    },
-    "greendoorlive.tv": {
-        "api": "greendoorlivetv.mymember.site",
-        "name": "The World Famous Green Door",
-    },
-    "heatheraustin.live": {
-        "api": "heather-austin.mymember.site",
-        "name": "Heather Austin",
-    },
-    "heavybondage4life.com": {
-        "api": "heavybondage4life.mymember.site",
-        "name": "Heavybondage4Life",
-    },
-    "hornyadventures.com": {
-        "api": "hornyadventurestv.mymember.site",
-        "name": "Horny Adventures",
-    },
-    "hornysilver.com": {
-        "api": "hornysilver.mymember.site",
-        "name": "Hornysilver",
-    },
-    "hotwifeheidihaze.com": {
-        "api": "heidi-haze.mymember.site",
-        "name": "Heidi Haze",
-    },
-    "islandboyvids.com": {
-        "api": "islandboyvids.mymember.site",
-        "name": "Island Boy Vids",
-    },
-    "josyblack.tv": {
-        "api": "josyblack.mymember.site",
-        "name": "Josy Black",
-    },
-    "juteandroses.com": {
-        "api": "juteandroses.mymember.site",
-        "name": "Jute and Roses",
-    },
-    "kingnoirexxx.com": {
-        "api": "kingnoirexxx.mymember.site",
-        "name": "King Noire",
-    },
-    "kinkography.com": {
-        "api": "kinkography.mymember.site",
-        "name": "Kinkography",
-    },
-    "kinkyponygirl.com": {
-        "api": "kinkyponygirl.mymember.site",
-        "name": "KinkyPonygirl",
-    },
-    "kinkyrubberdreams.com": {
-        "api": "kinkyrubberdreams.mymember.site",
-        "name": "Glowing Darkness",
-    },
-    "kitehkawasaki.com": {
-        "api": "kitehkawasaki.mymember.site",
-        "name": "Kiteh Kawasaki",
-    },
-    "labelladx.com": {
-        "api": "labelladx.mymember.site",
-        "name": "LaBellaDiablaX",
-    },
-    "lady-asmondena.com": {
-        "api": "ladyasmondena.mymember.site",
-        "name": "Lady Asmondena",
-    },
-    "lamodelsdoporn.com": {
-        "api": "lamodelsdoporn.mymember.site",
-        "name": "LA Models Do Porn",
-    },
-    "latexkittyxxx.com": {
-        "api": "latexkittyxxx.mymember.site",
-        "name": "Latexkittyxxx",
-    },
-    "latexlolanoir.com": {
-        "api": "latexlolanoir.mymember.site",
-        "name": "Lola Noir",
-    },
-    "latexrapturefans.com": {
-        "api": "latexrapturefans.mymember.site",
-        "name": "LatexRapture",
-    },
-    "letseatcakexx.com": {
-        "api": "letseatcakexx.mymember.site",
-        "name": "LetsEatCakeXx",
-    },
-    "loonerlanding.com": {
-        "api": "loonerlanding.mymember.site",
-        "name": "Looner Landing",
-    },
-    "lukespov.vip": {
-        "api": "lukespov.mymember.site",
-        "name": "Luke's POV",
-    },
-    "marvalstudio.com": {
-        "api": "marvalstudio.mymember.site",
-        "name": "MarValStudio",
-    },
-    "members.baebihel.com": {
-        "api": "baebi-hel.mymember.site",
-        "name": "Baebi Hel",
-    },
-    "michaelfittnation.com": {
-        "api": "michaelfittnation.mymember.site",
-        "name": "Michael Fitt",
-    },
-    "milenaangel.club": {
-        "api": "milenaangel.mymember.site",
-        "name": "MilenaAngel",
-    },
-    "mondofetiche.com": {
-        "api": "mondofetiche.mymember.site",
-        "name": "Mondo Fetiche",
-    },
-    "mrhappyendings.com": {
-        "api": "mrhappyendings.mymember.site",
-        "name": "Mr Happy Endings",
-    },
-    "mymember.site/Goddessjazzy": {
-        "api": "goddessjazzy.mymember.site",
-        "name": "Goddess Jazzy",
-    },
-    "mymember.site/androprince-cs-chamber/": {
-        "api": "androprince-cs-chamber.mymember.site",
-        "name": "AndroPrince C's Chamber",
-    },
-    "mymember.site/aoikamogawa": {
-        "api": "aoikamogawa.mymember.site",
-        "name": "Aoi Kamogawa",
-    },
-    "mymember.site/eroticious": {
-        "api": "eroticious.mymember.site",
-        "name": "eroticious",
-    },
-    "mymember.site/kyara-in-ropes": {
-        "api": "kyara-in-ropes.mymember.site",
-        "name": "Kyara in Ropes",
-    },
-    "mymember.site/latex-desire": {
-        "api": "latex-desire.mymember.site",
-        "name": "LatexDesire",
-    },
-    "mymember.site/linaroselina": {
-        "api": "linaroselina.mymember.site",
-        "name": "Lina Roselina",
-    },
-    "mymember.site/mr-rains-sexy-wrestling": {
-        "api": "mr-rains-sexy-wrestling.mymember.site",
-        "name": "Mr Rains Sexy Wrestling",
-    },
-    "mymember.site/officialemyang": {
-        "api": "officialemyang.mymember.site",
-        "name": "Official Em Yang",
-    },
-    "mymember.site/rubbobjectdoll": {
-        "api": "rubbobjectdoll.mymember.site",
-        "name": "RubbobjectDoll",
-    },
-    "nicoledupapillon.net": {
-        "api": "nicole-dupapillon.mymember.site",
-        "name": "Nicole DuPapillon",
-    },
-    "nikitzo.com": {
-        "api": "nikitzo.mymember.site",
-        "name": "NIKITZO",
-    },
-    "nikkidavisxo.com": {
-        "api": "nikkidavisxo.mymember.site",
-        "name": "NikkiDavisXO",
-    },
-    "nylon-encasement.com": {
-        "api": "nylon-encasement.mymember.site",
-        "name": "Nylon Encasement",
-    },
-    "peacockcouple.com": {
-        "api": "peacockcouple.mymember.site",
-        "name": "PeacockCouple",
-    },
-    "pedal-passion.com": {
-        "api": "pedal-passion.mymember.site",
-        "name": "Pedal Passion",
-    },
-    "pervfect.net": {
-        "api": "pervfect.mymember.site",
-        "name": "Pervfect",
-    },
-    "psilosirenxxx.com": {
-        "api": "psilosirenxxx.mymember.site",
-        "name": "PsiloSiren",
-    },
-    "riggsfilms.vip": {
-        "api": "riggsfilms.mymember.site",
-        "name": "Riggs Films",
-    },
-    "royalfetishxxx.com": {
-        "api": "royalfetishxxx.mymember.site",
-        "name": "RoyalFetishXXX",
-    },
-    "rubber-pervs.com": {
-        "api": "rubberpervs.mymember.site",
-        "name": "Rubber-Pervs",
-    },
-    "rubberdollemmalee.com": {
-        "api": "rubberdollemmalee.mymember.site",
-        "name": "Rubberdoll Emma Lee",
-    },
-    "sam-serenity.com": {
-        "api": "sam-serenity.mymember.site",
-        "name": "Sam Serenity",
-    },
-    "sexyhippies.com": {
-        "api": "sexyhippies.mymember.site",
-        "name": "Sexy Hippies",
-    },
-    "sephypink.com": {
-        "api": "sephypink.mymember.site",
-        "name": "Persephone Pink",
-    },
-    "shemalevalentina.com": {
-        "api": "shemalevalentina.mymember.site",
-        "name": "Shemale Valentina",
-    },
-    "slutwife.club": {
-        "api": "slutwife-club.mymember.site",
-        "name": "SLUTWIFE CLUB",
-    },
-    "spanishxbarbiiexxx.com": {
-        "api": "spanishxbarbiie.mymember.site",
-        "name": "Spanish Barbie",
-    },
-    "strong-men.com": {
-        "api": "strong-men.mymember.site",
-        "name": "Strong-Men",
-    },
-    "tabooseduction.com": {
-        "api": "tabooseduction.mymember.site",
-        "name": "TabooSeduction",
-    },
-    "taboosexstories4k.com": {
-        "api": "taboosexstories4k.mymember.site",
-        "name": "Taboo Sex Stories",
-    },
-    "the-strapon-site.com": {
-        "api": "thestraponsite.mymember.site",
-        "name": "The Strapon Site",
-    },
-    "theextortionarium.com": {
-        "api": "the-extortionarium.mymember.site",
-        "name": "The Extortionarium",
-    },
-    "thegoonhole.com": {
-        "api": "thegoonhole.mymember.site",
-        "name": "The Goonhole",
-    },
-    "thekandikjewel.com": {
-        "api": "thekandikjewel.mymember.site",
-        "name": "The Kandi K Jewel",
-    },
-    "unlimited.lovely-anita.com": {
-        "api": "lovelyanita.mymember.site",
-        "name": "Lovely Anita",
-    },
-    "whatlizannelikes.com": {
-        "api": "whatlizannelikes.mymember.site",
-        "name": "What Lizanne Likes",
-    },
-    "yourfitcrush.com": {
-        "api": "yourfitcrush.mymember.site",
-        "name": "Your Fit Crush",
-    },
-}
+def __api_request(scrape_url: str) -> dict:
+    """
+    Makes a GET request to the given URL and returns the JSON response.
+    Exits if the request fails or if the response status is not 200.
 
+    Parameters
+    ----------
+    scrape_url : str
+        The URL to fetch.
 
-def __api_request(url) -> dict:
-    log.debug(f"Fetching '{url}'")
+    Returns
+    -------
+    dict
+        The JSON response from the API.
+    """
+    log.debug(f"Fetching '{scrape_url}'")
     try:
-        response = session.get(url, timeout=(3, 6))
+        response = session.get(scrape_url, timeout=(3, 6))
     except requests.exceptions.RequestException as req_ex:
-        log.error(f"Error fetching '{url}': {req_ex}")
+        log.error(f"Error fetching '{scrape_url}': {req_ex}")
         sys.exit(-1)
 
     if response.status_code != 200:
-        log.error(f"Fetching '{url}' resulted in error status: {response.status_code}")
+        log.error(f"Fetching '{scrape_url}' resulted in error status: {response.status_code}")
         sys.exit(-1)
 
     data = response.json()
@@ -446,39 +51,115 @@ def __api_request(url) -> dict:
     return data
 
 
-def __parse_url(url: str) -> tuple[str, str]:
+def __parse_url(api_url: str, scraped_url: str) -> str:
     """
     Returns the studio name and the API URL corresponding to the given URL.
 
     Exits if the domain is not known or if the path does not conform to the expected format
+
+    Parameters
+    ----------
+    api_url : str
+        The base URL of the API.
+    scraped_url : str
+        The URL to parse.
+
+    Returns
+    -------
+    str
+        The API URL corresponding to the given URL.
     """
-    parsed = urlparse(url)
-
-    domain = re.sub(r"^www\.", "", parsed.netloc)
-    if domain == "mymember.site":
-        domain += "/" + parsed.path.split("/")[1]
-    if not (studio := _studios.get(domain)):
-        log.error(f"Domain {domain} not supported, URL: '{url}'")
-        sys.exit(-1)
-
+    parsed = urlparse(scraped_url)
     if not (
         match := re.match(
             r"^/(?P<type>videos|photosets)/(?P<id>\d+)(?:-(?P<name>.+))?$", parsed.path
         )
     ):
-        log.error(f"Unable to parse URL '{url}'")
+        log.error(f"Unable to parse URL '{scraped_url}'")
         sys.exit(-1)
 
     _type, _id, slug = match.groups()
 
-    _type = {"photosets": "photo-collections"}.get(_type, _type)
-
-    return studio["name"], f"https://{studio['api']}/api/{_type}/{_id}"
+    return f"{api_url}/{_type}/{_id}"
 
 
-def gallery_from_url(url: str) -> ScrapedGallery:
-    studio, api_url = __parse_url(url)
-    raw_gallery = __api_request(api_url)
+def __parse_api_url(scrape_url: str) -> str:
+    """
+    Fetches the page and extracts the subdomain meta tag to determine the API URL.
+    Exits if the page cannot be fetched or if the meta tag is not found.
+
+    Parameters
+    ----------
+    scrape_url : str
+        The URL of the page to fetch.
+
+    Returns
+    -------
+    str
+        The API URL extracted from the meta tag.
+    """
+    response = requests.get(scrape_url)
+    if response.status_code != 200:
+        log.error(f"Fetching '{scrape_url}' resulted in error status: {response.status_code}")
+        sys.exit(-1)
+
+    html = BeautifulSoup(response.content, "html.parser")
+    subdomain_tag = html.find("meta", {"name": "subdomain"})
+    if not subdomain_tag or "content" not in subdomain_tag.attrs:
+        log.error(f"Meta tag with name 'subdomain' not found in '{scrape_url}'")
+        sys.exit(-1)
+
+    return f"https://{subdomain_tag['content']}.mymember.site/api"
+
+
+def __fetch_studio(api_url: str, scrape_url: str) -> ScrapedStudio:
+    """
+    Fetches the studio information from the API.
+    Exits if the API request fails or if the site_info is not found in the response.
+
+    Parameters
+    ----------
+    api_url : str
+        The base URL of the API.
+    scrape_url : str
+        The original URL being scraped, used to construct the studio URL.
+
+    Returns
+    -------
+    ScrapedStudio
+        The scraped studio data.
+    """
+    response = __api_request(f"{api_url}/auth/init")
+    if "site_info" not in response:
+        log.error(f"site_info not found in '{api_url}/api/auth/init' response")
+        sys.exit(-1)
+
+    parsed_url = urlparse(scrape_url)
+    studio_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+    return ScrapedStudio(
+        name=response["site_info"].get("site_long_name", "MyMemberSite"),
+        url=studio_url,
+        image=response["site_info"].get("site_logo", ""),
+    )
+
+
+def gallery_from_url(gallery_url: str) -> ScrapedGallery:
+    """
+    Scrapes a gallery from the given URL.
+
+    Parameters
+    ----------
+    gallery_url : str
+        The URL of the gallery to scrape.
+
+    Returns
+    -------
+    ScrapedGallery
+        The scraped gallery data.
+    """
+    api_url: str = __parse_api_url(gallery_url)
+    studio: ScrapedStudio = __fetch_studio(api_url, gallery_url)
+    raw_gallery = __api_request(__parse_url(api_url, gallery_url))
 
     scraped: ScrapedGallery = {}
 
@@ -495,19 +176,33 @@ def gallery_from_url(url: str) -> ScrapedGallery:
         scraped["details"] = BeautifulSoup(details, "html.parser").get_text()
 
     if tags := raw_gallery.get("tags"):
-        scraped["tags"] = [{"name": t["name"]} for t in tags]
+        scraped["tags"] = [ScrapedTag(name=t["name"]) for t in tags]
 
     if cast := raw_gallery.get("casts"):
-        scraped["performers"] = [{"name": p["screen_name"]} for p in cast]
+        scraped["performers"] = [ScrapedPerformer(name=p["screen_name"]) for p in cast]
 
-    scraped["studio"] = {"name": studio}
+    scraped["studio"] = studio
 
     return scraped
 
 
-def scene_from_url(url: str) -> ScrapedScene:
-    studio, api_url = __parse_url(url)
-    raw_scene = __api_request(api_url)
+def scene_from_url(scene_url: str) -> ScrapedScene:
+    """
+    Scrapes a scene from the given URL.
+
+    Parameters
+    ----------
+    scene_url : str
+        The URL of the scene to scrape.
+
+    Returns
+    -------
+    ScrapedScene
+        The scraped scene data.
+    """
+    api_url: str = __parse_api_url(scene_url)
+    studio: ScrapedStudio = __fetch_studio(api_url, scene_url)
+    raw_scene = __api_request(__parse_url(api_url, scene_url))
 
     scraped: ScrapedScene = {}
 
@@ -524,15 +219,15 @@ def scene_from_url(url: str) -> ScrapedScene:
         scraped["details"] = BeautifulSoup(details, "html.parser").get_text()
 
     if tags := raw_scene.get("tags"):
-        scraped["tags"] = [{"name": t["name"]} for t in tags]
+        scraped["tags"] = [ScrapedTag(name=t["name"]) for t in tags]
 
     if cast := raw_scene.get("casts"):
-        scraped["performers"] = [{"name": p["screen_name"]} for p in cast]
+        scraped["performers"] = [ScrapedPerformer(name=p["screen_name"]) for p in cast]
 
     if image := dig(raw_scene, ("poster_src", "cover_photo")):
         scraped["image"] = image
 
-    scraped["studio"] = {"name": studio}
+    scraped["studio"] = studio
 
     return scraped
 
